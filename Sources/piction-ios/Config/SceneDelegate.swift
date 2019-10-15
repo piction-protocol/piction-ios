2073fd824920e2b99149eab25417f43f65fc8d80
jhSeo <jh.seo@battleent.com>
2019-10-15 18:07:11 +0900

    Change plist files path

---
+++ Sources/piction-ios/Config/SceneDelegate.swift
@@ -0,0 +1,85 @@
+//
+//  SceneDelegate.swift
+//  piction-ios
+//
+//  Created by jhseo on 15/10/2019.
+//  Copyright © 2019 Piction Network. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene // <-- Window Scene set to UIWindow
+
+        if UserDefaults.standard.string(forKey: "pincode") != nil {
+            let vc = CheckPincodeViewController.make(style: .initial)
+            window?.rootViewController = vc
+        } else {
+            let rootView = TabBarController()
+            window?.rootViewController = rootView
+        }
+        if #available(iOS 13.0, *) {
+            window?.backgroundColor = .systemBackground
+        } else {
+            window?.backgroundColor = .white
+        }
+        window?.makeKeyAndVisible()
+    }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if let topViewController = UIApplication.topViewController(), topViewController.isKind(of: SignUpViewController.self) {
+                _ = DeepLinkManager.executeDeepLink(with: url)
+            } else {
+                UIApplication.dismissAllPresentedController {
+                    _ = DeepLinkManager.executeDeepLink(with: url)
+                }
+            }
+        }
+    }
+
+    func windowScene(_ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        if let window = self.window {
+            AppShortcuts.performShortcut(window: window, shortcut: shortcutItem)
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
