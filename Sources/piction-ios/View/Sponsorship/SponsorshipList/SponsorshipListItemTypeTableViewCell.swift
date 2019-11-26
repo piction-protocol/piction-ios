d7c94524663368190ed572e2ee477d7c4691f82d
jhSeo <jh.seo@battleent.com>
2019-11-26 14:56:21 +0900

    Add date in sponsorship list screen

--- Sources/piction-ios/View/Sponsorship/SponsorshipList/SponsorshipListItemTypeTableViewCell.swift
+++ Sources/piction-ios/View/Sponsorship/SponsorshipList/SponsorshipListItemTypeTableViewCell.swift
@@ -34,6 +34,6 @@ final class SponsorshipListItemTypeTableViewCell: ReuseTableViewCell {
 
         amountLabel.text = "\(amount.commaRepresentation) PXL"
         messageLabel.text = LocalizedStrings.str_sponsorship_for.localized(with: creatorName ?? "")
-        dateLabel.text = createdAt?.toString(format: "hh:mm")
+        dateLabel.text = createdAt?.toString(format: "M/d\nh:m")
     }
 }
