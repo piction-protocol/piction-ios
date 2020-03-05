ff7b72b0bfb00c714e4d95f1a29f1cce55fb4dd2
jhSeo <jh.seo@battleent.com>
2020-03-05 12:11:36 +0900

    Add comment for SubscriptionListViewController

--- Sources/piction-ios/Views/Controller/SubscriptionListViewController.swift
+++ Sources/piction-ios/Views/Controller/SubscriptionListViewController.swift
@@ -14,6 +14,7 @@ import RxDataSources
 import UIScrollView_InfiniteScroll
 import PictionSDK
 
+// MARK: - UIViewController
 final class SubscriptionListViewController: UIViewController {
     var disposeBag = DisposeBag()
 
@@ -22,8 +23,11 @@ final class SubscriptionListViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
+            // 로그인이 되어 있지 않으면 scroll되지 않도록 함
             collectionView.isScrollEnabled = false
+            // pull to refresh 추가
             collectionView.refreshControl = refreshControl
+            // collectionView footer 추가
             collectionView.registerReusableView(ReuseCollectionReusableView.self, kind: .footer)
         }
     }
@@ -31,79 +35,43 @@ final class SubscriptionListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // emptyView의 height를 보여지는 화면의 height로 설정
         emptyView.frame.size.height = visibleHeight
     }
 
-    private func embedCustomEmptyViewController(style: CustomEmptyViewStyle) {
-        _ = emptyView.subviews.map { $0.removeFromSuperview() }
-        let vc = CustomEmptyViewController.make(style: style)
-        embed(vc, to: emptyView)
-        self.collectionView.reloadData()
-    }
-
-    private func configureDataSource() -> RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>> {
-        return RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>>(
-            configureCell: { dataSource, collectionView, indexPath, model in
-                let cell: SubscriptionListCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
-                cell.configure(with: model)
-                return cell
-        },
-        configureSupplementaryView: { [weak self] (dataSource, collectionView, kind, indexPath) in
-            guard let `self` = self else { return UICollectionReusableView() }
-            if (kind == UICollectionView.elementKindSectionFooter) {
-                let reusableView = collectionView.dequeueReusableView(ReuseCollectionReusableView.self, indexPath: indexPath, kind: .footer)
-
-                _ = reusableView.subviews.map { $0.removeFromSuperview() }
-                reusableView.addSubview(self.emptyView)
-                return reusableView
-            }
-            return UICollectionReusableView()
-        })
-    }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-
-        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            let cellCount: CGFloat = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad && view.frame.size.width == UIScreen.main.bounds.size.width ? 4 : 2
-            let width = (view.frame.size.width - 40 - (cellCount - 1) * 7) / cellCount
-            let height = width + 44
-            flowLayout.itemSize = CGSize(width: width, height: height)
-            flowLayout.invalidateLayout()
-        }
-        emptyView.frame.size.width = view.frame.size.width
-        emptyView.frame.size.height = visibleHeight
-    }
-
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        collectionView.setInfiniteScrollStyle()
+    deinit {
+        // 메모리 해제되는지 확인
+        print("[deinit] \(String(describing: type(of: self)))")
     }
 }
 
+// MARK: - ViewModelBindable
 extension SubscriptionListViewController: ViewModelBindable {
     typealias ViewModel = SubscriptionListViewModel
 
     func bindViewModel(viewModel: ViewModel) {
         let dataSource = configureDataSource()
 
+        // infiniteScroll이 동작할 때
         collectionView.addInfiniteScroll { [weak self] tableView in
             self?.viewModel?.loadNextTrigger.onNext(())
         }
+        // infiniteScroll이 동작하는 조건
         collectionView.setShouldShowInfiniteScrollHandler { [weak self] _ in
             return self?.viewModel?.shouldInfiniteScroll ?? false
         }
 
         let input = SubscriptionListViewModel.Input(
-            viewWillAppear: rx.viewWillAppear.asDriver(),
-            viewWillDisappear: rx.viewWillDisappear.asDriver(),
-            selectedIndexPath:
-            collectionView.rx.itemSelected.asDriver(),
-            refreshControlDidRefresh: refreshControl.rx.controlEvent(.valueChanged).asDriver()
+            viewWillAppear: rx.viewWillAppear.asDriver(), // 화면이 보여지기 전에
+            viewWillLayoutSubviews: rx.viewWillLayoutSubviews.asDriver(), // subview의 layout이 갱신되기 전에
+            traitCollectionDidChange: rx.traitCollectionDidChange.asDriver(), // 일반/다크모드 전환 시
+            selectedIndexPath: collectionView.rx.itemSelected.asDriver(), // collectionView의 item을 눌렀을 때
+            refreshControlDidRefresh: refreshControl.rx.controlEvent(.valueChanged).asDriver() // pull to refresh 액션 시
         )
 
         let output = viewModel.build(input: input)
 
+        // 화면이 보여지기 전에 NavigationBar, infiniteScroll 설정
         output
             .viewWillAppear
             .drive(onNext: { [weak self] in
@@ -112,6 +80,23 @@ extension SubscriptionListViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // subview의 layout이 갱신되기 전에
+        output
+            .viewWillLayoutSubviews
+            .drive(onNext: { [weak self] in
+                self?.changeLayoutSubviews()
+            })
+            .disposed(by: disposeBag)
+
+        // 일반/다크모드 전환 시 Infinite scroll 색 변경
+        output
+            .traitCollectionDidChange
+            .drive(onNext: { [weak self] in
+                self?.collectionView.setInfiniteScrollStyle()
+            })
+            .disposed(by: disposeBag)
+
+        // 구독중인 project의 데이터를 collectionView에 출력
         output
             .subscriptionList
             .do(onNext: { [weak self] _ in
@@ -122,6 +107,7 @@ extension SubscriptionListViewController: ViewModelBindable {
             .bind(to: collectionView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
 
+        // 구독중인 project의 테이터를 출력 후 infiniteScroll 로딩 해제
         output
             .subscriptionList
             .drive(onNext: { [weak self] subscriptions in
@@ -131,6 +117,7 @@ extension SubscriptionListViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // emptyView 출력
         output
             .embedEmptyViewController
             .drive(onNext: { [weak self] style in
@@ -142,38 +129,46 @@ extension SubscriptionListViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // collectionView의 item을 선택하면 project 화면으로 push
         output
-            .openProjectViewController
-            .drive(onNext: { [weak self] indexPath in
-                guard let uri = dataSource[indexPath].uri else { return }
-                self?.openProjectViewController(uri: uri)
+            .selectedIndexPath
+            .map { dataSource[$0].uri }
+            .flatMap(Driver.from)
+            .map { .project(uri: $0) } // project 화면으로 push
+            .drive(onNext: { [weak self] in
+                self?.openView(type: $0, openType: .push)
             })
             .disposed(by: disposeBag)
 
+        // pull to refresh 로딩 및 해제
         output
             .isFetching
             .drive(refreshControl.rx.isRefreshing)
             .disposed(by: disposeBag)
 
+        // 로딩 뷰
         output
             .activityIndicator
             .loadingActivity()
             .disposed(by: disposeBag)
 
+        // 네트워크 오류 시 에러 팝업 출력
         output
             .showErrorPopup
             .drive(onNext: { [weak self] in
-                self?.collectionView.finishInfiniteScroll()
-                Toast.loadingActivity(false)
+                self?.collectionView.finishInfiniteScroll() // infiniteScroll 로딩 중이면 로딩 해제
+                Toast.loadingActivity(false) // 로딩 뷰 로딩 중이면 로딩 해제
                 self?.showPopup(
                     title: LocalizationKey.popup_title_network_error.localized(),
                     message: LocalizationKey.msg_api_internal_server_error.localized(),
                     action: [LocalizationKey.retry.localized(), LocalizationKey.cancel.localized()]) { [weak self] in
+                        // 다시 로딩
                         self?.viewModel?.loadRetryTrigger.onNext(())
                     }
             })
             .disposed(by: disposeBag)
 
+        // session이 변경되면 모든 뷰를 pop (root로 이동)
         output
             .refreshSession
             .drive(onNext: { [weak self] in
@@ -183,7 +178,9 @@ extension SubscriptionListViewController: ViewModelBindable {
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
 extension SubscriptionListViewController: UICollectionViewDelegateFlowLayout {
+    // footer size 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         if self.emptyView.subviews.count > 0 {
             return CGSize(width: SCREEN_W, height: emptyView.frame.size.height)
@@ -192,13 +189,62 @@ extension SubscriptionListViewController: UICollectionViewDelegateFlowLayout {
         }
     }
 
+    // 각 section의 padding 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         if self.emptyView.subviews.count > 0 {
             return UIEdgeInsets.zero
         } else {
             return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-
         }
     }
 }
 
+// MARK: - DataSource
+extension SubscriptionListViewController {
+    private func configureDataSource() -> RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>> {
+        return RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>>(
+            // cell 설정
+            configureCell: { dataSource, collectionView, indexPath, model in
+                let cell: SubscriptionListCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+                cell.configure(with: model)
+                return cell
+            },
+            // header, footer 설정
+            configureSupplementaryView: { [weak self] (dataSource, collectionView, kind, indexPath) in
+                guard let `self` = self else { return UICollectionReusableView() }
+                if (kind == UICollectionView.elementKindSectionFooter) {
+                    let reusableView = collectionView.dequeueReusableView(ReuseCollectionReusableView.self, indexPath: indexPath, kind: .footer)
+
+                    // subview를 모두 제거 하고 emptyView를 add
+                    _ = reusableView.subviews.map { $0.removeFromSuperview() }
+                    reusableView.addSubview(self.emptyView)
+                    return reusableView
+                }
+                return UICollectionReusableView()
+            })
+    }
+}
+
+// MARK: - Private Method
+extension SubscriptionListViewController {
+    // Pad에서 가로/세로모드 변경 시 cell size 변경 (pad 가로모드에서는 한줄에 4개의 cell을 보여주도록 함)
+    private func changeLayoutSubviews() {
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let cellCount: CGFloat = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad && view.frame.size.width == UIScreen.main.bounds.size.width ? 4 : 2
+            let width = (view.frame.size.width - 40 - (cellCount - 1) * 7) / cellCount
+            let height = width + 44
+            flowLayout.itemSize = CGSize(width: width, height: height)
+            flowLayout.invalidateLayout()
+        }
+        emptyView.frame.size.width = view.frame.size.width
+        emptyView.frame.size.height = visibleHeight
+    }
+
+    // emptyView를 embed
+    private func embedCustomEmptyViewController(style: CustomEmptyViewStyle) {
+        _ = emptyView.subviews.map { $0.removeFromSuperview() }
+        let vc = CustomEmptyViewController.make(style: style)
+        embed(vc, to: emptyView)
+        self.collectionView.reloadData()
+    }
+}
