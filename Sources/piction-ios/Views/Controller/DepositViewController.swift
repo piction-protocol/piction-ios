83763eb8fa19ac26f0b29e68c19eaecd8d1fa93e
jhSeo <jh.seo@battleent.com>
2020-03-05 11:55:49 +0900

    Add comment for DepositViewController

--- Sources/piction-ios/Views/Controller/DepositViewController.swift
+++ Sources/piction-ios/Views/Controller/DepositViewController.swift
@@ -11,6 +11,7 @@ import RxSwift
 import RxCocoa
 import ViewModelBindable
 
+// MARK: - UIViewController
 final class DepositViewController: UIViewController {
     var disposeBag = DisposeBag()
 
@@ -23,11 +24,14 @@ final class DepositViewController: UIViewController {
         didSet {
             let attributedStr = NSMutableAttributedString(string: LocalizationKey.str_deposit_guide_1.localized())
 
+            // paragraphStyle
             let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.headIndent = 10
-            paragraphStyle.lineHeightMultiple = 1.5
+            paragraphStyle.headIndent = 10 // 들여쓰기
+            paragraphStyle.lineHeightMultiple = 1.5 // 라인 간격
 
+            // 폰트 사이즈
             attributedStr.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 12), range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_1.localized()))
+            // paragraphStyle 적용
             attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_1.localized()))
             depositGuidePiction1Label.attributedText = attributedStr
         }
@@ -36,11 +40,14 @@ final class DepositViewController: UIViewController {
         didSet {
             let attributedStr = NSMutableAttributedString(string: LocalizationKey.str_deposit_guide_2.localized())
 
+            // paragraphStyle
             let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.headIndent = 10
-            paragraphStyle.lineHeightMultiple = 1.5
+            paragraphStyle.headIndent = 10 // 들여쓰기
+            paragraphStyle.lineHeightMultiple = 1.5 // 라인 간격
 
+            // 폰트 사이즈
             attributedStr.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 12), range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_2.localized()))
+            // paragraphStyle 적용
             attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_2.localized()))
             depositGuidePiction2Label.attributedText = attributedStr
         }
@@ -49,12 +56,15 @@ final class DepositViewController: UIViewController {
         didSet {
             let attributedStr = NSMutableAttributedString(string: LocalizationKey.str_deposit_guide_3.localized())
 
+            // paragraphStyle
             let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.headIndent = 10
-            paragraphStyle.lineHeightMultiple = 1.5
+            paragraphStyle.headIndent = 10 // 들여쓰기
+            paragraphStyle.lineHeightMultiple = 1.5 // 라인 간격
 
-            attributedStr.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 12), range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_3.localized()))
-            attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_3.localized()))
+            // 폰트 사이즈
+            attributedStr.addAttribute(.font, value: UIFont.systemFont(ofSize: 12), range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_3.localized()))
+            // paragraphStyle 적용
+            attributedStr.addAttribute(.paragraphStyle, value: paragraphStyle, range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_3.localized()))
             depositGuide1Label.attributedText = attributedStr
         }
     }
@@ -62,30 +72,39 @@ final class DepositViewController: UIViewController {
         didSet {
             let attributedStr = NSMutableAttributedString(string: LocalizationKey.str_deposit_guide_4_piction.localized())
 
+            // paragraphStyle
             let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.headIndent = 10
-            paragraphStyle.lineHeightMultiple = 1.5
+            paragraphStyle.headIndent = 10 // 들여쓰기
+            paragraphStyle.lineHeightMultiple = 1.5 // 라인 간격
 
-            attributedStr.addAttribute(NSAttributedString.Key.font, value: UIFont.systemFont(ofSize: 12), range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_4_piction.localized()))
-            attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_4_piction.localized()))
+             // 폰트 사이즈
+            attributedStr.addAttribute(.font, value: UIFont.systemFont(ofSize: 12), range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_4_piction.localized()))
+             // paragraphStyle 적용
+            attributedStr.addAttribute(.paragraphStyle, value: paragraphStyle, range: attributedStr.mutableString.range(of: LocalizationKey.str_deposit_guide_4_piction.localized()))
             depositGuide2Label.attributedText = attributedStr
         }
     }
     @IBOutlet weak var copyAddressButton: UIButton!
+
+    deinit {
+        // 메모리 해제되는지 확인
+        print("[deinit] \(String(describing: type(of: self)))")
+    }
 }
 
+// MARK: - ViewModelBindable
 extension DepositViewController: ViewModelBindable {
     typealias ViewModel = DepositViewModel
 
     func bindViewModel(viewModel: ViewModel) {
-
         let input = DepositViewModel.Input(
-            viewWillAppear: rx.viewWillAppear.asDriver(),
-            copyBtnDidTap: copyAddressButton.rx.tap.asDriver()
+            viewWillAppear: rx.viewWillAppear.asDriver(), // 화면이 보여지기 전에
+            copyBtnDidTap: copyAddressButton.rx.tap.asDriver() // 주소 복사 버튼 눌렀을 때
         )
 
         let output = viewModel.build(input: input)
 
+        // 화면이 보여지기 전에 NavigationBar 설정
         output
             .viewWillAppear
             .drive(onNext: { [weak self] _ in
@@ -93,13 +112,16 @@ extension DepositViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // 유저 정보를 불러와서 설정
         output
             .userInfo
-            .drive(onNext: { [weak self] userInfo in
-                self?.idLabel.text = LocalizationKey.str_wallet_address.localized(with: userInfo.loginId ?? "")
-            })
+            .map { $0.loginId }
+            .flatMap(Driver.from)
+            .map { LocalizationKey.str_wallet_address.localized(with: $0) }
+            .drive(idLabel.rx.text)
             .disposed(by: disposeBag)
 
+        // wallet 정보를 불러와서 설정
         output
             .walletInfo
             .drive(onNext: { [weak self] walletInfo in
@@ -109,6 +131,7 @@ extension DepositViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // 주소 복사
         output
             .copyAddress
             .drive(onNext: { address in
@@ -117,19 +140,22 @@ extension DepositViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // 네트워크 오류 시 에러 팝업 출력
         output
             .showErrorPopup
             .drive(onNext: { [weak self] in
-                Toast.loadingActivity(false)
+                Toast.loadingActivity(false) // 로딩 뷰 로딩 중이면 로딩 해제
                 self?.showPopup(
                     title: LocalizationKey.popup_title_network_error.localized(),
                     message: LocalizationKey.msg_api_internal_server_error.localized(),
                     action: [LocalizationKey.retry.localized(), LocalizationKey.cancel.localized()]) { [weak self] in
+                        // 다시 로딩
                         self?.viewModel?.loadRetryTrigger.onNext(())
                     }
             })
             .disposed(by: disposeBag)
 
+        // 로딩 뷰
         output
             .activityIndicator
             .loadingActivity()
