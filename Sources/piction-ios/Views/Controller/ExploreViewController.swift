072d80455fbfb419b55e5857372b26dd083c7c4f
jhSeo <jh.seo@battleent.com>
2020-03-05 11:41:38 +0900

    Add comment for ExploreViewController

--- Sources/piction-ios/Views/Controller/ExploreViewController.swift
+++ Sources/piction-ios/Views/Controller/ExploreViewController.swift
@@ -14,6 +14,7 @@ import RxDataSources
 import UIScrollView_InfiniteScroll
 import PictionSDK
 
+// MARK: - UIViewController
 final class ExploreViewController: UIViewController {
     var disposeBag = DisposeBag()
 
@@ -26,43 +27,217 @@ final class ExploreViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
+            // pull to refresh 추가
             collectionView.refreshControl = refreshControl
+            // ProjectListCollectionViewCell은 공통으로 사용하여 storyboard가 아닌 여기서 등록
             collectionView.registerXib(ProjectListCollectionViewCell.self)
+            // collectionView header 추가
             collectionView.registerReusableView(ReuseCollectionReusableView.self, kind: .header)
+            // collectionView footer 추가
             collectionView.registerReusableView(ReuseCollectionReusableView.self, kind: .footer)
+            // UI가 완전히 보여지기 전까지는 숨김
             collectionView.isHidden = true
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // emptyView의 height를 보여지는 화면의 height로 설정
         emptyView.frame.size.height = visibleHeight
 
         self.configureSearchController()
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        if #available(iOS 13, *) {
+    deinit {
+        // 메모리 해제되는지 확인
+        print("[deinit] \(String(describing: type(of: self)))")
+    }
+}
+
+// MARK: - ViewModelBindable
+extension ExploreViewController: ViewModelBindable {
+    typealias ViewModel = ExploreViewModel
+
+    func bindViewModel(viewModel: ViewModel) {
+        let dataSource = configureDataSource()
+
+        // infiniteScroll이 동작할 때
+        collectionView.addInfiniteScroll { [weak self] tableView in
+            self?.viewModel?.loadNextTrigger.onNext(())
+        }
+        // infiniteScroll이 동작하는 조건
+        collectionView.setShouldShowInfiniteScrollHandler { [weak self] _ in
+            return self?.viewModel?.shouldInfiniteScroll ?? false
+        }
+
+        let input = ExploreViewModel.Input(
+            viewWillAppear: rx.viewWillAppear.asDriver(), // 화면이 보여지기 전에
+            viewDidAppear: rx.viewDidAppear.asDriver(), // 화면이 보여질 때
+            viewWillLayoutSubviews: rx.viewWillLayoutSubviews.asDriver(), // subview의 layout이 갱신되기 전에
+            traitCollectionDidChange: rx.traitCollectionDidChange.asDriver(), // 일반/다크모드 전환 시
+            selectedIndexPath: collectionView.rx.itemSelected.asDriver(), // collectionView의 item을 눌렀을 때
+            refreshControlDidRefresh: refreshControl.rx.controlEvent(.valueChanged).asDriver() // pull to refresh 액션 시
+        )
+
+        let output = viewModel.build(input: input)
+
+        // 화면이 보여지기 전에 NavigationBar 설정
+        output
+            .viewWillAppear
+            .drive(onNext: { [weak self] in
+                self?.navigationController?.configureNavigationBar(transparent: false, shadow: false)
+                self?.collectionView.setInfiniteScrollStyle()
+            })
+            .disposed(by: disposeBag)
+
+        // 화면이 보여질 때
+        output
+            .viewDidAppear
+            .drive(onNext: { [weak self] in
+                // iOS 13 이전 버전에서는 화면이 보여질 때 navigation설정을 다시 해줘야 함
+                if #available(iOS 13, *) {
+                } else {
+                    self?.navigationItem.hidesSearchBarWhenScrolling = true
+                    self?.navigationController?.configureNavigationBar(transparent: false, shadow: false)
+                }
+            })
+            .disposed(by: disposeBag)
+
+        // subview의 layout이 갱신되기 전에
+        output
+            .viewWillLayoutSubviews
+            .drive(onNext: { [weak self] in
+                self?.changeLayoutSubviews()
+            })
+            .disposed(by: disposeBag)
+
+        // 일반/다크모드 전환 시 Infinite scroll 색 변경
+        output
+            .traitCollectionDidChange
+            .drive(onNext: { [weak self] in
+                self?.collectionView.setInfiniteScrollStyle()
+            })
+            .disposed(by: disposeBag)
+
+        // 카테고리 리스트 embed
+        output
+            .embedCategoryListViewController
+            .drive(onNext: { [weak self] in
+                self?.embedCategoryListViewController()
+            })
+            .disposed(by: disposeBag)
+
+        // 프로젝트 리스트를 collectionView에 출력
+        output
+            .projectList
+            .do(onNext: { [weak self] _ in
+                _ = self?.emptyView.subviews.map { $0.removeFromSuperview() }
+            })
+            .drive { $0 }
+            .map { [SectionModel(model: "", items: $0)] }
+            .bind(to: collectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+
+        // 프로젝트 리스트를 출력 후 infiniteScroll 로딩 해제
+        output
+            .projectList
+            .drive(onNext: { [weak self] _ in
+                self?.collectionView.layoutIfNeeded()
+                self?.collectionView.finishInfiniteScroll()
+            })
+            .disposed(by: disposeBag)
+
+        // collectionView의 item 선택 시 project 화면으로 push
+        output
+            .selectedIndexPath
+            .map { dataSource[$0].uri }
+            .flatMap(Driver.from)
+            .map { .project(uri: $0) }
+            .drive(onNext: { [weak self] in
+                self?.openView(type: $0, openType: .push)
+            })
+            .disposed(by: disposeBag)
+
+        // pull to refresh 로딩 및 해제
+        output
+            .isFetching
+            .drive(refreshControl.rx.isRefreshing)
+            .disposed(by: disposeBag)
+
+        // 로딩 뷰
+        output
+            .activityIndicator
+            .loadingActivity()
+            .disposed(by: disposeBag)
+
+        // 네트워크 오류 시 에러 팝업 출력
+        output
+            .showErrorPopup
+            .drive(onNext: { [weak self] in
+                self?.collectionView.finishInfiniteScroll() // infiniteScroll 로딩 중이면 로딩 해제
+                Toast.loadingActivity(false) // 로딩 뷰 로딩 중이면 로딩 해제
+                self?.showPopup(
+                    title: LocalizationKey.popup_title_network_error.localized(),
+                    message: LocalizationKey.msg_api_internal_server_error.localized(),
+                    action: [LocalizationKey.retry.localized(), LocalizationKey.cancel.localized()]) { [weak self] in
+                        // 다시 로딩
+                        self?.viewModel?.loadRetryTrigger.onNext(())
+                    }
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - CategoryListViewDelegate
+extension ExploreViewController: CategoryListViewDelegate {
+    // categoryList의 로딩이 완료되면 collectionView를 리로딩하고 collectionView를 숨김해제 함
+    func loadComplete() {
+        collectionView.reloadData()
+        collectionView.isHidden = false
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ExploreViewController: UICollectionViewDelegateFlowLayout {
+    // header size 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+
+        guard let headerCollectionView = exploreHeaderView?.collectionView else { return CGSize(width: view.frame.size.width, height: 50) }
+        let width = view.frame.size.width
+        let height = headerCollectionView.contentSize.height
+        return CGSize(width: width, height: height)
+    }
+
+    // footer size 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        if self.emptyView.subviews.count > 0 {
+            return CGSize(width: SCREEN_W, height: emptyView.frame.size.height)
         } else {
-            self.navigationItem.hidesSearchBarWhenScrolling = true
-            self.navigationController?.configureNavigationBar(transparent: false, shadow: false)
+            return CGSize.zero
         }
     }
 
-    private func embedCategoryListViewController() {
-        self.exploreHeaderView = CategoryListViewController.make()
-        self.exploreHeaderView?.delegate = self
+    // 각 section의 padding 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if self.emptyView.subviews.count > 0 {
+            return UIEdgeInsets.zero
+        } else {
+            return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        }
     }
+}
 
+// MARK: - DataSource
+extension ExploreViewController {
     private func configureDataSource() -> RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>> {
         return RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>>(
-
+            // cell 설정
             configureCell: { dataSource, collectionView, indexPath, model in
                 let cell: ProjectListCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
                 cell.configure(with: model)
                 return cell
             },
+            // header, footer 설정
             configureSupplementaryView: { [weak self] (dataSource, collectionView, kind, indexPath) in
                 guard let `self` = self else { return UICollectionReusableView() }
 
@@ -70,6 +245,7 @@ final class ExploreViewController: UIViewController {
                 case UICollectionView.elementKindSectionHeader:
                     let reusableView = collectionView.dequeueReusableView(ReuseCollectionReusableView.self, indexPath: indexPath, kind: .header)
 
+                    // subview가 없는 경우에만 header를 embed
                     if reusableView.subviews.isEmpty {
                         if let exploreHeaderView = self.exploreHeaderView {
                             self.embed(exploreHeaderView, to: reusableView)
@@ -80,6 +256,7 @@ final class ExploreViewController: UIViewController {
                 case UICollectionView.elementKindSectionFooter:
                     let reusableView = collectionView.dequeueReusableView(ReuseCollectionReusableView.self, indexPath: indexPath, kind: .footer)
 
+                    // subview를 모두 제거 하고 emptyView를 add
                     _ = reusableView.subviews.map { $0.removeFromSuperview() }
                     reusableView.addSubview(self.emptyView)
                     return reusableView
@@ -88,25 +265,11 @@ final class ExploreViewController: UIViewController {
                 }
             })
     }
+}
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-
-        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            let cellCount: CGFloat = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad && view.frame.size.width == UIScreen.main.bounds.size.width ? 4 : 2
-            let width = (view.frame.size.width - 40 - (cellCount - 1) * 7) / cellCount
-            let height = width + 44
-            flowLayout.itemSize = CGSize(width: width, height: height)
-            flowLayout.invalidateLayout()
-            collectionView.layoutIfNeeded()
-        }
-    }
-
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        collectionView.setInfiniteScrollStyle()
-    }
-
+// MARK: - Private Method
+extension ExploreViewController {
+    // searchController 설정
     private func configureSearchController() {
         self.searchController = UISearchController(searchResultsController: self.searchResultsController)
 
@@ -126,125 +289,22 @@ final class ExploreViewController: UIViewController {
 
         self.searchController?.searchBar.placeholder = LocalizationKey.hint_project_and_tag_search.localized()
     }
-}
 
-extension ExploreViewController: ViewModelBindable {
-    typealias ViewModel = ExploreViewModel
-
-    func bindViewModel(viewModel: ViewModel) {
-        let dataSource = configureDataSource()
-
-        collectionView.addInfiniteScroll { [weak self] tableView in
-            self?.viewModel?.loadNextTrigger.onNext(())
-        }
-        collectionView.setShouldShowInfiniteScrollHandler { [weak self] _ in
-            return self?.viewModel?.shouldInfiniteScroll ?? false
-        }
-
-        let input = ExploreViewModel.Input(
-            viewWillAppear: rx.viewWillAppear.asDriver(),
-            viewWillDisappear: rx.viewWillDisappear.asDriver(),
-            selectedIndexPath:
-            collectionView.rx.itemSelected.asDriver(),
-            refreshControlDidRefresh: refreshControl.rx.controlEvent(.valueChanged).asDriver()
-        )
-
-        let output = viewModel.build(input: input)
-
-        output
-            .viewWillAppear
-            .drive(onNext: { [weak self] in
-                self?.navigationController?.configureNavigationBar(transparent: false, shadow: false)
-                self?.collectionView.setInfiniteScrollStyle()
-            })
-            .disposed(by: disposeBag)
-
-        output
-            .embedCategoryListViewController
-            .drive(onNext: { self.embedCategoryListViewController() })
-            .disposed(by: disposeBag)
-
-        output
-            .projectList
-            .do(onNext: { [weak self] _ in
-                _ = self?.emptyView.subviews.map { $0.removeFromSuperview() }
-            })
-            .drive { $0 }
-            .map { [SectionModel(model: "", items: $0)] }
-            .bind(to: collectionView.rx.items(dataSource: dataSource))
-            .disposed(by: disposeBag)
-
-        output
-            .projectList
-            .drive(onNext: { [weak self] _ in
-                self?.collectionView.layoutIfNeeded()
-                self?.collectionView.finishInfiniteScroll()
-            })
-            .disposed(by: disposeBag)
-
-        output
-            .selectedIndexPath
-            .map { dataSource[$0].uri }
-            .filter { $0 != nil }
-            .flatMap(Driver.from)
-            .drive(onNext: { self.openProjectViewController(uri: $0) })
-            .disposed(by: disposeBag)
-
-        output
-            .isFetching
-            .drive(refreshControl.rx.isRefreshing)
-            .disposed(by: disposeBag)
-
-        output
-            .activityIndicator
-            .loadingActivity()
-            .disposed(by: disposeBag)
-
-        output
-            .showErrorPopup
-            .drive(onNext: { [weak self] in
-                self?.collectionView.finishInfiniteScroll()
-                Toast.loadingActivity(false)
-                self?.showPopup(
-                    title: LocalizationKey.popup_title_network_error.localized(),
-                    message: LocalizationKey.msg_api_internal_server_error.localized(),
-                    action: [LocalizationKey.retry.localized(), LocalizationKey.cancel.localized()]) { [weak self] in
-                        self?.viewModel?.loadRetryTrigger.onNext(())
-                    }
-            })
-            .disposed(by: disposeBag)
-    }
-}
-
-extension ExploreViewController: CategoryListViewDelegate {
-    func loadComplete() {
-        collectionView.reloadData()
-        collectionView.isHidden = false
-    }
-}
-
-extension ExploreViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-
-        guard let headerCollectionView = exploreHeaderView?.collectionView else { return CGSize(width: view.frame.size.width, height: 50) }
-        let width = view.frame.size.width
-        let height = headerCollectionView.contentSize.height
-        return CGSize(width: width, height: height)
+    // 카테고리 목록을 header에 embed
+    private func embedCategoryListViewController() {
+        self.exploreHeaderView = CategoryListViewController.make()
+        self.exploreHeaderView?.delegate = self
     }
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        if self.emptyView.subviews.count > 0 {
-            return CGSize(width: SCREEN_W, height: emptyView.frame.size.height)
-        } else {
-            return CGSize.zero
-        }
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        if self.emptyView.subviews.count > 0 {
-            return UIEdgeInsets.zero
-        } else {
-            return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    // Pad에서 가로/세로모드 변경 시 cell size 변경 (pad 가로모드에서는 한줄에 4개의 cell을 보여주도록 함)
+    private func changeLayoutSubviews() {
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let cellCount: CGFloat = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad && view.frame.size.width == UIScreen.main.bounds.size.width ? 4 : 2
+            let width = (view.frame.size.width - 40 - (cellCount - 1) * 7) / cellCount
+            let height = width + 44
+            flowLayout.itemSize = CGSize(width: width, height: height)
+            flowLayout.invalidateLayout()
+            collectionView.layoutIfNeeded()
         }
     }
 }
