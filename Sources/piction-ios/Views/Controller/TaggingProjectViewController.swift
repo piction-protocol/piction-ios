e33b327b3d2ad490fbebad5138bc8809cc441687
jhSeo <jh.seo@battleent.com>
2020-03-05 11:10:04 +0900

    Add comment for TaggingProjectViewController

--- Sources/piction-ios/Views/Controller/TaggingProjectViewController.swift
+++ Sources/piction-ios/Views/Controller/TaggingProjectViewController.swift
@@ -14,6 +14,7 @@ import RxDataSources
 import UIScrollView_InfiniteScroll
 import PictionSDK
 
+// MARK: - UIViewController
 final class TaggingProjectViewController: UIViewController {
     var disposeBag = DisposeBag()
 
@@ -22,8 +23,11 @@ final class TaggingProjectViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
+            // pull to refresh 추가
             collectionView.refreshControl = refreshControl
+            // ProjectListCollectionViewCell은 공통으로 사용하여 storyboard가 아닌 여기서 등록
             collectionView.registerXib(ProjectListCollectionViewCell.self)
+            // collectionView footer 추가
             collectionView.registerReusableView(ReuseCollectionReusableView.self, kind: .footer)
         }
     }
@@ -31,86 +35,50 @@ final class TaggingProjectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // emptyView의 height를 보여지는 화면의 height로 설정
         emptyView.frame.size.height = visibleHeight
     }
 
-    private func embedCustomEmptyViewController(style: CustomEmptyViewStyle) {
-        _ = emptyView.subviews.map { $0.removeFromSuperview() }
-        let vc = CustomEmptyViewController.make(style: style)
-        embed(vc, to: emptyView)
-        self.collectionView.reloadData()
-    }
-
-    private func configureDataSource() -> RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>> {
-        return RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>>(
-            configureCell: { dataSource, collectionView, indexPath, model in
-                let cell: ProjectListCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
-                cell.configure(with: model)
-                return cell
-        },
-        configureSupplementaryView: { [weak self] (dataSource, collectionView, kind, indexPath) in
-            guard let `self` = self else { return UICollectionReusableView() }
-            if (kind == UICollectionView.elementKindSectionFooter) {
-                let reusableView = collectionView.dequeueReusableView(ReuseCollectionReusableView.self, indexPath: indexPath, kind: .footer)
-
-                _ = reusableView.subviews.map { $0.removeFromSuperview() }
-                reusableView.addSubview(self.emptyView)
-                return reusableView
-            }
-            return UICollectionReusableView()
-        })
-    }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-
-        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            let cellCount: CGFloat = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad && view.frame.size.width == UIScreen.main.bounds.size.width ? 4 : 2
-            let width = (view.frame.size.width - 40 - (cellCount - 1) * 7) / cellCount 
-            let height = width + 44
-            flowLayout.itemSize = CGSize(width: width, height: height)
-            flowLayout.invalidateLayout()
-        }
-        emptyView.frame.size.width = view.frame.size.width
-        emptyView.frame.size.height = visibleHeight
-    }
-
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        collectionView.setInfiniteScrollStyle()
+    deinit {
+        // 메모리 해제되는지 확인
+        print("[deinit] \(String(describing: type(of: self)))")
     }
 }
 
+// MARK: - ViewModelBindable
 extension TaggingProjectViewController: ViewModelBindable {
     typealias ViewModel = TaggingProjectViewModel
 
     func bindViewModel(viewModel: ViewModel) {
         let dataSource = configureDataSource()
 
+        // infiniteScroll이 동작할 때
         collectionView.addInfiniteScroll { [weak self] tableView in
             self?.viewModel?.loadNextTrigger.onNext(())
         }
+        // infiniteScroll이 동작하는 조건
         collectionView.setShouldShowInfiniteScrollHandler { [weak self] _ in
             return self?.viewModel?.shouldInfiniteScroll ?? false
         }
 
         let input = TaggingProjectViewModel.Input(
-            viewWillAppear: rx.viewWillAppear.asDriver(),
-            viewWillDisappear: rx.viewWillDisappear.asDriver(),
-            selectedIndexPath:
-            collectionView.rx.itemSelected.asDriver(),
-            refreshControlDidRefresh: refreshControl.rx.controlEvent(.valueChanged).asDriver()
+            viewWillAppear: rx.viewWillAppear.asDriver(), // 화면이 보여지기 전에
+            viewWillLayoutSubviews: rx.viewWillLayoutSubviews.asDriver(), // subview의 layout이 갱신되기 전에
+            traitCollectionDidChange: rx.traitCollectionDidChange.asDriver(), // 일반/다크모드 전환 시
+            selectedIndexPath: collectionView.rx.itemSelected.asDriver(), // collectionView의 item을 눌렀을 때
+            refreshControlDidRefresh: refreshControl.rx.controlEvent(.valueChanged).asDriver() // pull to refresh 액션 시
         )
 
         let output = viewModel.build(input: input)
 
+        // 네비게이션 타이틀을 #태그로 설정
         output
             .navigationTitle
-            .drive(onNext: { [weak self] title in
-                self?.navigationItem.title = "#\(title)"
-            })
+            .map { "#\($0)" }
+            .drive(navigationItem.rx.title)
             .disposed(by: disposeBag)
 
+        // 화면이 보여지기 전에 NavigationBar, InfiniteScrollStyle 설정
         output
             .viewWillAppear
             .drive(onNext: { [weak self] in
@@ -119,16 +87,34 @@ extension TaggingProjectViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // subview의 layout이 갱신되기 전에
+        output
+            .viewWillLayoutSubviews
+            .drive(onNext: { [weak self] in
+                self?.changeLayoutSubviews()
+            })
+            .disposed(by: disposeBag)
+
+        // 일반/다크모드 전환 시 Infinite scroll 색 변경
+        output
+            .traitCollectionDidChange
+            .drive(onNext: { [weak self] in
+                self?.collectionView.setInfiniteScrollStyle()
+            })
+            .disposed(by: disposeBag)
+
+        // tag된 프로젝트 목록을 collectionView에 출력
         output
             .taggingProjectList
             .do(onNext: { [weak self] _ in
-                _ = self?.emptyView.subviews.map { $0.removeFromSuperview() }
+                _ = self?.emptyView.subviews.map { $0.removeFromSuperview() } // emptyView 제거
             })
             .drive { $0 }
             .map { [SectionModel(model: "", items: $0)] }
             .bind(to: collectionView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
 
+        // tag된 프로젝트 목록 출력 후 infiniteScroll 로딩 해제
         output
             .taggingProjectList
             .drive(onNext: { [weak self] _ in
@@ -137,41 +123,48 @@ extension TaggingProjectViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // collectionView의 item을 선택했을 때
         output
-            .openProjectViewController
-            .drive(onNext: { [weak self] indexPath in
-                guard let uri = dataSource[indexPath].uri else { return }
-                self?.openProjectViewController(uri: uri)
+            .selectedIndexPath
+            .map { dataSource[$0].uri }
+            .flatMap(Driver.from)
+            .map { .project(uri: $0) }
+            .drive(onNext: { [weak self] in
+                self?.openView(type: $0, openType: .push)
             })
             .disposed(by: disposeBag)
 
+        // emptyView 출력
         output
             .embedEmptyViewController
-            .drive(onNext: { [weak self] style in
-                guard let `self` = self else { return }
-                self.embedCustomEmptyViewController(style: style)
+            .drive(onNext: { [weak self] in
+                self?.embedCustomEmptyViewController(style: $0)
             })
             .disposed(by: disposeBag)
 
+        // pull to refresh 로딩 및 해제
         output
             .isFetching
             .drive(refreshControl.rx.isRefreshing)
             .disposed(by: disposeBag)
 
+        // 네트워크 에러 시 에러 팝업 출력
         output
             .showErrorPopup
             .drive(onNext: { [weak self] in
-                self?.collectionView.finishInfiniteScroll()
-                Toast.loadingActivity(false)
+                self?.collectionView.finishInfiniteScroll() // infiniteScroll 로딩 중이면 로딩 해제
+                Toast.loadingActivity(false) // 로딩 뷰 로딩 중이면 로딩 해제
                 self?.showPopup(
                     title: LocalizationKey.popup_title_network_error.localized(),
                     message: LocalizationKey.msg_api_internal_server_error.localized(),
                     action: [LocalizationKey.retry.localized(), LocalizationKey.cancel.localized()]) { [weak self] in
-                    self?.viewModel?.loadRetryTrigger.onNext(())
-                }
+                        // 다시 로딩
+                        self?.viewModel?.loadRetryTrigger.onNext(())
+                    }
             })
             .disposed(by: disposeBag)
 
+        // 로딩 뷰
         output
             .activityIndicator
             .loadingActivity()
@@ -179,7 +172,9 @@ extension TaggingProjectViewController: ViewModelBindable {
     }
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
 extension TaggingProjectViewController: UICollectionViewDelegateFlowLayout {
+    // footer의 size 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         if self.emptyView.subviews.count > 0 {
             return CGSize(width: SCREEN_W, height: emptyView.frame.size.height)
@@ -188,6 +183,7 @@ extension TaggingProjectViewController: UICollectionViewDelegateFlowLayout {
         }
     }
 
+    // 각 section의 padding 설정
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         if self.emptyView.subviews.count > 0 {
             return UIEdgeInsets.zero
@@ -198,3 +194,52 @@ extension TaggingProjectViewController: UICollectionViewDelegateFlowLayout {
     }
 }
 
+// MARK: - DataSource
+extension TaggingProjectViewController {
+    private func configureDataSource() -> RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>> {
+        return RxCollectionViewSectionedReloadDataSource<SectionModel<String, ProjectModel>>(
+            // cell 설정
+            configureCell: { dataSource, collectionView, indexPath, model in
+                let cell: ProjectListCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+                cell.configure(with: model)
+                return cell
+        },
+        // header, footer 설정
+        configureSupplementaryView: { [weak self] (dataSource, collectionView, kind, indexPath) in
+            guard let `self` = self else { return UICollectionReusableView() }
+            if (kind == UICollectionView.elementKindSectionFooter) {
+                let reusableView = collectionView.dequeueReusableView(ReuseCollectionReusableView.self, indexPath: indexPath, kind: .footer)
+
+                // subview를 모두 제거 하고 emptyView를 add
+                _ = reusableView.subviews.map { $0.removeFromSuperview() }
+                reusableView.addSubview(self.emptyView)
+                return reusableView
+            }
+            return UICollectionReusableView()
+        })
+    }
+}
+
+// MARK: - Private Method
+extension TaggingProjectViewController {
+    // emptyView를 embed
+    private func embedCustomEmptyViewController(style: CustomEmptyViewStyle) {
+        _ = emptyView.subviews.map { $0.removeFromSuperview() }
+        let vc = CustomEmptyViewController.make(style: style)
+        embed(vc, to: emptyView)
+        self.collectionView.reloadData()
+    }
+
+    // Pad에서 가로/세로모드 변경 시 cell size 변경 (pad 가로모드에서는 한줄에 4개의 cell을 보여주도록 함)
+    private func changeLayoutSubviews() {
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            let cellCount: CGFloat = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad && view.frame.size.width == UIScreen.main.bounds.size.width ? 4 : 2
+            let width = (view.frame.size.width - 40 - (cellCount - 1) * 7) / cellCount
+            let height = width + 44
+            flowLayout.itemSize = CGSize(width: width, height: height)
+            flowLayout.invalidateLayout()
+        }
+        emptyView.frame.size.width = view.frame.size.width
+        emptyView.frame.size.height = visibleHeight
+    }
+}
