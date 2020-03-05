fbe4e5a62cf97f07e1adc2c2e01c7e19efae3e49
jhSeo <jh.seo@battleent.com>
2020-03-05 10:42:48 +0900

    Add comment for SignInViewController

--- Sources/piction-ios/Views/Controller/SignInViewController.swift
+++ Sources/piction-ios/Views/Controller/SignInViewController.swift
@@ -12,6 +12,7 @@ import RxCocoa
 import ViewModelBindable
 import SafariServices
 
+// MARK: - UIViewController
 final class SignInViewController: UIViewController {
     var disposeBag = DisposeBag()
 
@@ -32,75 +33,77 @@ final class SignInViewController: UIViewController {
         }
     }
 
+    // password textfield에서 return key 누르면 login 되도록 함
     private let keyboardReturnSignIn = PublishSubject<Void>()
 
-    @IBAction func tapGesture(_ sender: Any) {
-        view.endEditing(true)
+    deinit {
+        // 메모리 해제되는지 확인
+        print("[deinit] \(String(describing: type(of: self)))")
     }
 }
 
+// MARK: - ViewModelBindable
 extension SignInViewController: ViewModelBindable {
-
     typealias ViewModel = SignInViewModel
 
     func bindViewModel(viewModel: ViewModel) {
-
         let input = SignInViewModel.Input(
-            viewWillAppear: rx.viewWillAppear.asDriver(),
-            viewWillDisappear: rx.viewWillDisappear.asDriver(),
-            signInBtnDidTap: Driver.merge(signInButton.rx.tap.asDriver(), keyboardReturnSignIn.asDriver(onErrorDriveWith: .empty())),
-            signUpBtnDidTap: signUpButton.rx.tap.asDriver(),
-            loginIdTextFieldDidInput: loginIdInputView.inputTextField.rx.text.orEmpty.asDriver(),
-            passwordTextFieldDidInput: passwordInputView.inputTextField.rx.text.orEmpty.asDriver(),
-            findPasswordBtnDidTap: findPasswordButton.rx.tap.asDriver(),
-            closeBtnDidTap: closeButton.rx.tap.asDriver()
+            viewWillAppear: rx.viewWillAppear.asDriver(), // 화면이 보여지기 전에
+            viewWillDisappear: rx.viewWillDisappear.asDriver(), // 화면이 사라지기 전에
+            signInBtnDidTap: Driver.merge(signInButton.rx.tap.asDriver(), keyboardReturnSignIn.asDriver(onErrorDriveWith: .empty())), // 로그인 버튼이나 키보드의 return 버튼을 눌렀을 때
+            signUpBtnDidTap: signUpButton.rx.tap.asDriver(), // 회원가입 버튼을 눌렀을 때
+            loginIdTextFieldDidInput: loginIdInputView.inputTextField.rx.text.orEmpty.asDriver(), // id textfield를 입력했을 때
+            passwordTextFieldDidInput: passwordInputView.inputTextField.rx.text.orEmpty.asDriver(), // password textfield를 입력했을 때
+            findPasswordBtnDidTap: findPasswordButton.rx.tap.asDriver(), // 비밀번호 찾기 버튼을 눌렀을 때
+            closeBtnDidTap: closeButton.rx.tap.asDriver() // 닫기 버튼을 눌렀을 때
         )
 
         let output = viewModel.build(input: input)
 
+        // 화면이 보여지기 전에 NavigationBar 설정
         output
             .viewWillAppear
-            .drive(onNext: { [weak self] _ in
+            .drive(onNext: { [weak self] in
                 self?.navigationController?.configureNavigationBar(transparent: false, shadow: true)
             })
             .disposed(by: disposeBag)
 
+        // 화면이 사라지기전에 viewModel에서 keyboard를 숨기고 disposed하기 위함
         output
             .viewWillDisappear
-            .drive(onNext: { _ in
-            })
+            .drive()
             .disposed(by: disposeBag)
 
+        // 이미 로그인 되어있는데 딥링크 등을 통해 진입할 경우 화면을 닫고 토스트 출력
         output
             .userInfo
-            .drive(onNext: { [weak self] userInfo in
-                if userInfo.loginId != "" {
-                    self?.dismiss(animated: true, completion: {
-                        Toast.showToast(LocalizationKey.msg_already_sign_in.localized())
-                    })
+            .filter { $0.loginId != "" }
+            .drive(onNext: { [weak self] _ in
+                self?.dismiss(animated: true) {
+                    Toast.showToast(LocalizationKey.msg_already_sign_in.localized())
                 }
             })
             .disposed(by: disposeBag)
 
+        // 회원가입 화면으로 push
         output
             .openSignUpViewController
+            .map { .signUp }
             .drive(onNext: { [weak self] in
-                self?.openSignUpViewController()
+                self?.openView(type: $0, openType: .push)
             })
             .disposed(by: disposeBag)
 
+        // 비밀번호 찾기는 safari로 open
         output
             .openFindPassword
-            .drive(onNext: { _ in
+            .drive(onNext: { [weak self] in
                 let stagingPath = AppInfo.isStaging ? "staging." : ""
-
-                if let url = URL(string: "https://\(stagingPath)piction.network/forgot_password") {
-                    let safariViewController = SFSafariViewController(url: url)
-                    self.present(safariViewController, animated: true, completion: nil)
-                }
+                self?.openSafariViewController(url: "https://\(stagingPath)piction.network/forgot_password")
             })
             .disposed(by: disposeBag)
 
+        // keyboard가 나타나거나 사라질때 scrollView의 크기 조정
         output
             .keyboardWillChangeFrame
             .drive(onNext: { [weak self] changedFrameInfo in
@@ -121,22 +124,25 @@ extension SignInViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // 로딩 뷰
         output
             .activityIndicator
             .loadingActivity()
             .disposed(by: disposeBag)
 
+        // 로그인 후 pincode 등록이 안되있으면 화면을 닫은 후 pincode 등록 화면 출력
         output
             .dismissViewController
-            .drive(onNext: { [weak self] keychainEmpty in
-                self?.dismiss(animated: true, completion: { [weak self] in
-                    if keychainEmpty {
-                        self?.openRegisterPincodeViewController()
+            .drive(onNext: { [weak self] pincodeEmpty in
+                self?.dismiss(animated: true) {
+                    if pincodeEmpty {
+                        self?.openView(type: .registerPincode, openType: .present)
                     }
-                })
+                }
             })
             .disposed(by: disposeBag)
 
+        // 에러 메시지를 각 field 밑에 출력
         output
             .errorMsg
             .drive(onNext: { [weak self] error in
@@ -151,24 +157,35 @@ extension SignInViewController: ViewModelBindable {
             })
             .disposed(by: disposeBag)
 
+        // 토스트 메시지 출력 (키보드에 가리기 때문에 키보드 숨긴 후 출력)
         output
             .toastMessage
             .do(onNext: { [weak self] message in
-                self?.loginIdInputView.inputTextField.resignFirstResponder()
-                self?.passwordInputView.inputTextField.resignFirstResponder()
+                self?.view.endEditing(true)
             })
             .showToast()
             .disposed(by: disposeBag)
     }
 }
 
+// MARK: - IBAction
+extension SignInViewController {
+    // 화면 tap 시 키보드 숨기기
+    @IBAction func tapGesture(_ sender: Any) {
+        view.endEditing(true)
+    }
+}
+
+// MARK: - DynamicInputViewDelegate
 extension SignInViewController: DynamicInputViewDelegate {
+    // 키보드의 return 키 눌렀을 때 다음 textField로 이동
     func returnKeyAction(_ textField: UITextField) {
         if textField === loginIdInputView.inputTextField {
             passwordInputView.inputTextField.becomeFirstResponder()
         } else {
             passwordInputView.inputTextField.resignFirstResponder()
-            self.keyboardReturnSignIn.onNext(())
+            // 마지막 textField는 편하게 바로 로그인 할 수 있도록 함
+            keyboardReturnSignIn.onNext(())
         }
     }
 }
