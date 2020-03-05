a9b8d0076c61eece1cb41b129f2958ceb9b42f9d
jhSeo <jh.seo@battleent.com>
2020-03-05 10:38:08 +0900

    Add comment for RegsiterPincodeViewController

--- Sources/piction-ios/Views/Controller/RegisterPincodeViewController.swift
+++ Sources/piction-ios/Views/Controller/RegisterPincodeViewController.swift
@@ -11,6 +11,7 @@ import RxSwift
 import RxCocoa
 import ViewModelBindable
 
+// MARK: - UIViewController
 final class RegisterPincodeViewController: UIViewController {
     var disposeBag = DisposeBag()
 
@@ -25,46 +26,40 @@ final class RegisterPincodeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // present 타입의경우 viewDidLoad에서 navigation을 설정
         self.navigationController?.configureNavigationBar(transparent: true, shadow: false)
     }
 
-    private func openRecommendPopup() {
-        let alertController = UIAlertController(title: LocalizationKey.popup_title_pincode_create.localized(), message: LocalizationKey.msg_pincode_reg_warning.localized(), preferredStyle: .alert)
-        let cancelButton = UIAlertAction(title: LocalizationKey.continue_go.localized(), style: .default) { _ in
-        }
-        let confirmButton = UIAlertAction(title: LocalizationKey.pass.localized(), style: .default) { [weak self] _ in
-            self?.dismiss(animated: true)
-        }
-
-        alertController.addAction(confirmButton)
-        alertController.addAction(cancelButton)
-
-        self.present(alertController, animated: true, completion: nil)
+    deinit {
+        // 메모리 해제되는지 확인
+        print("[deinit] \(String(describing: type(of: self)))")
     }
 }
 
+// MARK: - ViewModelBindable
 extension RegisterPincodeViewController: ViewModelBindable {
-
     typealias ViewModel = RegisterPincodeViewModel
 
     func bindViewModel(viewModel: ViewModel) {
-
         let input = RegisterPincodeViewModel.Input(
-            viewWillAppear: rx.viewWillAppear.asDriver(),
-            pincodeTextFieldDidInput: pincodeTextField.rx.text.orEmpty.asDriver(),
-            closeBtnDidTap: closeButton.rx.tap.asDriver()
+            viewWillAppear: rx.viewWillAppear.asDriver(), // 화면이 보여지기 전에
+            pincodeTextFieldDidInput: pincodeTextField.rx.text.orEmpty.asDriver(), // pincode textfield에 입력했을 때
+            closeBtnDidTap: closeButton.rx.tap.asDriver() // 닫기 버튼을 눌렀을 때
         )
 
         let output = viewModel.build(input: input)
 
+        // 화면이 보여지기 전에
         output
             .viewWillAppear
             .drive(onNext: { [weak self] in
-                self?.navigationController?.configureNavigationBar(transparent: true, shadow: false)
+                // 키보드 출력
                 self?.pincodeTextField.becomeFirstResponder()
             })
             .disposed(by: disposeBag)
 
+        // pincode textfield에 값이 입력될 때
         output
             .pincodeText
             .drive(onNext: { [weak self] inputPincode in
@@ -119,13 +114,16 @@ extension RegisterPincodeViewController: ViewModelBindable {
                     self?.pincode5View.backgroundColor = .pictionBlue
                     self?.pincode6View.backgroundColor = .pictionBlue
                     self?.pincodeTextField.text = ""
-                    self?.openConfirmPincodeViewController(inputPincode: inputPincode)
+
+                    // 등록한 pincode를 가지고 pincode 확인 화면으로 push
+                    self?.openView(type: .confirmPincode(inputPincode: inputPincode), openType: .push)
                 default:
                     break
                 }
             })
             .disposed(by: disposeBag)
 
+        // 닫기 버튼 누르면 pincode 설정 권장 팝업 출력
         output
             .openRecommendPopup
             .drive(onNext: { [weak self] in
@@ -135,12 +133,14 @@ extension RegisterPincodeViewController: ViewModelBindable {
     }
 }
 
+// MARK: - UITextFieldDelegate
 extension RegisterPincodeViewController: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         if (string.count == 0) {
             return true
         }
 
+        // 숫자만 입력 가능하도록 함
         if (textField == self.pincodeTextField) {
             let cs = CharacterSet(charactersIn: "0123456789")
             let filtered = string.components(separatedBy: cs).filter {  !$0.isEmpty }
@@ -149,5 +149,22 @@ extension RegisterPincodeViewController: UITextFieldDelegate {
             return (string != str)
         }
         return true
+    }
+}
+
+// MARK: - Private Method
+extension RegisterPincodeViewController {
+    // pincode 설정 권장 팝업
+    private func openRecommendPopup() {
+        let alertController = UIAlertController(title: LocalizationKey.popup_title_pincode_create.localized(), message: LocalizationKey.msg_pincode_reg_warning.localized(), preferredStyle: .alert)
+        let cancelButton = UIAlertAction(title: LocalizationKey.continue_go.localized(), style: .default)
+        let confirmButton = UIAlertAction(title: LocalizationKey.pass.localized(), style: .default) { _ in
+            self.dismiss(animated: true)
+        }
+
+        alertController.addAction(confirmButton)
+        alertController.addAction(cancelButton)
+
+        self.present(alertController, animated: true, completion: nil)
     }
 }
