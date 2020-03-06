8a6b83f74b0bcef4bd8e1bc94eb07e352051054d
jhSeo <jh.seo@battleent.com>
2020-03-06 12:45:20 +0900

    Add comment for UserInfoViewModel

--- Sources/piction-ios/ViewModels/UserInfoViewModel.swift
+++ Sources/piction-ios/ViewModels/UserInfoViewModel.swift
@@ -11,6 +11,7 @@ import RxSwift
 import RxCocoa
 import PictionSDK
 
+// MARK: - ViewModel
 final class UserInfoViewModel: InjectableViewModel {
 
     typealias Dependency = (
@@ -22,53 +23,77 @@ final class UserInfoViewModel: InjectableViewModel {
     init(dependency: Dependency) {
         (updater) = dependency
     }
+}
 
+// MARK: - Input & Output
+extension UserInfoViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
     }
-
     struct Output {
         let viewWillAppear: Driver<Void>
         let userInfo: Driver<UserModel>
         let walletInfo: Driver<WalletModel>
         let toastMessage: Driver<String>
     }
+}
 
+// MARK: - ViewModel Build
+extension UserInfoViewModel {
     func build(input: Input) -> Output {
-        let initialLoad = input.viewWillAppear.asObservable().take(1).asDriver(onErrorDriveWith: .empty())
+        
+        // 최초 진입 시
+        let initialLoad = input.viewWillAppear
+            .asObservable()
+            .take(1)
+            .asDriver(onErrorDriveWith: .empty())
 
-        let refreshSession = updater.refreshSession.asDriver(onErrorDriveWith: .empty())
-        let refreshAmount = updater.refreshAmount.asDriver(onErrorDriveWith: .empty())
+        // 세션 갱신 시
+        let refreshSession = updater.refreshSession
+            .asDriver(onErrorDriveWith: .empty())
 
+        // 금액 갱신 시
+        let refreshAmount = updater.refreshAmount
+            .asDriver(onErrorDriveWith: .empty())
+
+        // 유저 정보 호출
         let userInfoAction = Driver.merge(initialLoad, refreshSession, refreshAmount)
             .map { UserAPI.me }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
+        // 유저 정보 호출 성공 시
+        let userInfoSuccess = userInfoAction.elements
+            .map { try? $0.map(to: UserModel.self) }
+            .flatMap(Driver.from)
+
+        // 유저 정보 호출 에러 시
         let userInfoError = userInfoAction.error
             .map { $0 as? ErrorType }
             .map { $0?.message }
             .flatMap(Driver.from)
 
-        let userInfoSuccess = userInfoAction.elements
-            .map { try? $0.map(to: UserModel.self) }
-            .flatMap(Driver.from)
-
+        // 지갑 정보 호출
         let walletInfoAction = Driver.merge(initialLoad, refreshSession, refreshAmount)
             .map { WalletAPI.get }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
+        // 지갑 정보 호출 성공 시
+        let walletInfoSuccess = walletInfoAction.elements
+            .map { try? $0.map(to: WalletModel.self) }
+            .flatMap(Driver.from)
+
+        // 지갑 정보 호출 에러 시
         let walletInfoError = walletInfoAction.error
             .map { $0 as? ErrorType }
             .map { $0?.message }
             .flatMap(Driver.from)
 
-        let walletInfoSuccess = walletInfoAction.elements
-            .map { try? $0.map(to: WalletModel.self) }
-            .flatMap(Driver.from)
-
-        let toastMessage = Driver.merge(userInfoError, walletInfoError)
+        // 토스트 메시지
+        let toastMessage = Driver.merge(
+            userInfoError,
+            walletInfoError)
 
         return Output(
             viewWillAppear: input.viewWillAppear,
