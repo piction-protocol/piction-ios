5cb9f9dff64dbe3d1e6ceff5cd66d11f4f76337d
jhSeo <jh.seo@battleent.com>
2020-03-06 15:10:51 +0900

    Add SelfSizingCollectionView

---
+++ Sources/piction-ios/Helper/SelfSizingCollectionView/SelfSizingCollectionView.swift
@@ -0,0 +1,40 @@
+//
+//  SelfSizingCollectionView.swift
+//  piction-ios
+//
+//  Created by jhseo on 2020/03/05.
+//  Copyright © 2020 Piction Network. All rights reserved.
+//
+
+import UIKit
+
+class SelfSizingCollectionView: UICollectionView {
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        isScrollEnabled = false
+    }
+
+    override var contentSize: CGSize {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override func reloadData() {
+        super.reloadData()
+        self.invalidateIntrinsicContentSize()
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return contentSize
+    }
+}
