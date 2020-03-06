e0dd9157685eab459f9399f1a9d1f6e100574501
jhSeo <jh.seo@battleent.com>
2020-03-06 12:40:17 +0900

    Add comment for CheckPincodeViewModel

--- Sources/piction-ios/ViewModels/CheckPincodeViewModel.swift
+++ Sources/piction-ios/ViewModels/CheckPincodeViewModel.swift
@@ -11,11 +11,12 @@ import RxCocoa
 import PictionSDK
 
 enum CheckPincodeStyle {
-    case initial
-    case check
-    case change
+    case initial // 앱 최초 진입 시 인증
+    case check // 결제 등 인증이 필요한 경우
+    case change // pincode를 변경하기 전에 인증
 }
 
+// MARK: - ViewModel
 final class CheckPincodeViewModel: InjectableViewModel {
 
     typealias Dependency = (
@@ -31,48 +32,71 @@ final class CheckPincodeViewModel: InjectableViewModel {
     init(dependency: Dependency) {
         (firebaseManager, keychainManager, style) = dependency
     }
+}
 
+// MARK: - Input & Output
+extension CheckPincodeViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
         let pincodeTextFieldDidInput: Driver<String>
         let closeBtnDidTap: Driver<Void>
         let signout: Driver<Void>
     }
-
     struct Output {
-        let viewWillAppear: Driver<CheckPincodeStyle>
+        let viewWillAppear: Driver<Void>
+        let closeBtnStyle: Driver<CheckPincodeStyle>
         let inputPincode: Driver<(String, String)>
         let dismissViewController: Driver<Void>
     }
+}
 
+// MARK: - ViewModel Build
+extension CheckPincodeViewModel {
     func build(input: Input) -> Output {
         let (firebaseManager, keychainManager, style) = (self.firebaseManager, self.keychainManager, self.style)
 
+        // 화면이 보여지기 전에
         let viewWillAppear = input.viewWillAppear
-            .map { style }
             .do(onNext: { _ in
+                // analytics screen event
                 firebaseManager.screenName("PIN인증")
             })
 
-        let loadPage = input.viewWillAppear.asObservable().take(1).asDriver(onErrorDriveWith: .empty())
+        // 닫기 버튼 스타일
+        let closeBtnStyle = input.viewWillAppear
+            .map { style }
 
-        let currentPincode = loadPage
+        // 최초 진입 시
+        let initialLoad = input.viewWillAppear
+            .asObservable()
+            .take(1)
+            .asDriver(onErrorDriveWith: .empty())
+
+        // 현재 저장된 핀코드
+        let currentPincode = initialLoad
             .map { keychainManager.get(key: .pincode) }
 
+        // 로그아웃
         let signOutAction = input.signout
             .map { SessionAPI.delete }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
+        // 로그아웃 성공 시
         let signOutSuccess = signOutAction.elements
             .map { _ in Void() }
 
-        let dismissViewController = Driver.merge(signOutSuccess, input.closeBtnDidTap)
+        // 로그아웃 성공하거나 닫기버튼 누를 때 dismiss
+        let dismissViewController = Driver.merge(
+            signOutSuccess,
+            input.closeBtnDidTap)
 
+        // 핀코드 입력 시 현재 핀코드와 함께 전달
         let inputPincode = Driver.combineLatest(currentPincode, input.pincodeTextFieldDidInput)
 
         return Output(
             viewWillAppear: viewWillAppear,
+            closeBtnStyle: closeBtnStyle,
             inputPincode: inputPincode,
             dismissViewController: dismissViewController
         )
