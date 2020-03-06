98347afa227276dda2746dd8eabf18bdf059c784
jhSeo <jh.seo@battleent.com>
2020-03-06 12:40:57 +0900

    Add comment for CreatorProfileViewModel

--- Sources/piction-ios/ViewModels/CreatorProfileViewModel.swift
+++ Sources/piction-ios/ViewModels/CreatorProfileViewModel.swift
@@ -10,6 +10,7 @@ import RxSwift
 import RxCocoa
 import PictionSDK
 
+// MARK: - ViewModel
 final class CreatorProfileViewModel: InjectableViewModel {
 
     typealias Dependency = (
@@ -23,61 +24,81 @@ final class CreatorProfileViewModel: InjectableViewModel {
     init(dependency: Dependency) {
         (firebaseManager, loginId) = dependency
     }
+}
+
+// MARK: - Input & Output
+extension CreatorProfileViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
-        let viewWillDisappear: Driver<Void>
+        let viewWillLayoutSubviews: Driver<Void>
         let selectedIndexPath: Driver<IndexPath>
     }
-
     struct Output {
         let viewWillAppear: Driver<Void>
-        let viewWillDisappear: Driver<Void>
+        let viewWillLayoutSubviews: Driver<Void>
         let embedCreatorProfileHeaderViewController: Driver<String>
         let creatorProjectList: Driver<[ProjectModel]>
         let selectedIndexPath: Driver<IndexPath>
         let activityIndicator: Driver<Bool>
-        let dismissViewController: Driver<String>
+        let popViewController: Driver<String>
     }
+}
 
+// MARK: - ViewModel Build
+extension CreatorProfileViewModel {
     func build(input: Input) -> Output {
         let (firebaseManager, loginId) = (self.firebaseManager, self.loginId)
 
+        // 화면이 보여지기 전에
         let viewWillAppear = input.viewWillAppear
             .do(onNext: { _ in
+                // analytics screen event
                 firebaseManager.screenName("크리에이터상세_\(loginId)")
             })
 
-        let initialLoad = input.viewWillAppear.asObservable().take(1).asDriver(onErrorDriveWith: .empty())
+        // 최초 진입 시
+        let initialLoad = input.viewWillAppear
+            .asObservable()
+            .take(1)
+            .asDriver(onErrorDriveWith: .empty())
 
+        // 최초 진입 시
+        // header embed
         let embedCreatorProfileHeaderViewController = initialLoad
             .map { self.loginId }
 
+        // 최초 진입 시
+        // 크리에이터 정보 호출
         let creatorProjectAction = initialLoad
             .map { CreatorProfileAPI.getCreatorProfile(loginId: loginId) }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
+        // 크리에이터 프로젝트 호출 성공 시
         let creatorProjectSuccess = creatorProjectAction.elements
             .map { try? $0.map(to: [ProjectModel].self) }
             .flatMap(Driver.from)
 
+        // 크리에이터 프로젝트 호출 에러 시
         let creatorProjectError = creatorProjectAction.error
             .map { _ in [ProjectModel.from([:])!] }
 
-        let dismissViewController = Driver.merge(creatorProjectSuccess, creatorProjectError)
+        // 크리에이터 프로젝트가 없을 경우 pop
+        let popViewController = Driver.merge(creatorProjectSuccess, creatorProjectError)
             .filter { $0.isEmpty }
             .map { _ in LocalizationKey.msg_creator_not_found.localized() }
 
+        // 로딩 뷰
         let activityIndicator = creatorProjectAction.isExecuting
 
         return Output(
             viewWillAppear: viewWillAppear,
-            viewWillDisappear: input.viewWillDisappear,
+            viewWillLayoutSubviews: input.viewWillLayoutSubviews,
             embedCreatorProfileHeaderViewController: embedCreatorProfileHeaderViewController,
             creatorProjectList: creatorProjectSuccess,
             selectedIndexPath: input.selectedIndexPath,
             activityIndicator: activityIndicator,
-            dismissViewController: dismissViewController
+            popViewController: popViewController
         )
     }
 }
