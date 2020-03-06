2f43c20148aed6c6d71bfd9c5b545917dd280b28
jhSeo <jh.seo@battleent.com>
2020-03-06 12:43:46 +0900

    Add comment for SignInViewModel

--- Sources/piction-ios/ViewModels/SignInViewModel.swift
+++ Sources/piction-ios/ViewModels/SignInViewModel.swift
@@ -10,6 +10,7 @@ import RxSwift
 import RxCocoa
 import PictionSDK
 
+// MARK: - ViewModel
 final class SignInViewModel: InjectableViewModel {
 
     typealias Dependency = (
@@ -27,7 +28,10 @@ final class SignInViewModel: InjectableViewModel {
     init(dependency: Dependency) {
         (firebaseManager, updater, keyboardManager, keychainManager) = dependency
     }
+}
 
+// MARK: - Input & Output
+extension SignInViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
         let viewWillDisappear: Driver<Void>
@@ -38,7 +42,6 @@ final class SignInViewModel: InjectableViewModel {
         let findPasswordBtnDidTap: Driver<Void>
         let closeBtnDidTap: Driver<Void>
     }
-
     struct Output {
         let viewWillAppear: Driver<Void>
         let viewWillDisappear: Driver<Void>
@@ -51,41 +54,72 @@ final class SignInViewModel: InjectableViewModel {
         let errorMsg: Driver<ErrorModel>
         let toastMessage: Driver<String>
     }
+}
 
+// MARK: - ViewModel Build
+extension SignInViewModel {
     func build(input: Input) -> Output {
         let (firebaseManager, updater, keyboardManager, keychainManager) = (self.firebaseManager, self.updater, self.keyboardManager, self.keychainManager)
 
+        // 화면이 보여지기 전에
         let viewWillAppear = input.viewWillAppear
             .do(onNext: { _ in
+                // analytics screen event
                 firebaseManager.screenName("로그인")
+                // 키보드가 올라오는지 모니터링
                 keyboardManager.beginMonitoring()
             })
 
+        // 화면이 사라지기 전에
         let viewWillDisappear = input.viewWillDisappear
             .do(onNext: { _ in
+                // 키보드 모니터링 중단
                 keyboardManager.stopMonitoring()
             })
 
-        let initialLoad = input.viewWillAppear.asObservable().take(1).asDriver(onErrorDriveWith: .empty())
+        // 최초 진입 시
+        let initialLoad = input.viewWillAppear
+            .asObservable()
+            .take(1)
+            .asDriver(onErrorDriveWith: .empty())
 
+        // 유저 정보 호출
         let userInfoAction = initialLoad
             .map { UserAPI.me }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
+        // 유저 정보 호출 성공 시
         let userInfoSuccess = userInfoAction.elements
             .map { try? $0.map(to: UserModel.self) }
             .flatMap(Driver.from)
 
-        let signInInfo = Driver.combineLatest(input.loginIdTextFieldDidInput, input.passwordTextFieldDidInput) { (loginId: $0, password: $1) }
+        // 입력한 유저 정보
+        let signInInfo = Driver.combineLatest(
+            input.loginIdTextFieldDidInput,
+            input.passwordTextFieldDidInput)
+            { (loginId: $0, password: $1) }
 
-        let signInButtonAction = input.signInBtnDidTap
+        // 로그인 버튼 눌렀을 때 세션 생성 호출
+        let signInAction = input.signInBtnDidTap
             .withLatestFrom(signInInfo)
             .map { SessionAPI.create(loginId: $0.loginId, password: $0.password, rememberme: true) }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
-        let signInError = signInButtonAction.error
+        // 세션 생성 성공 시
+        let signInSuccess = signInAction.elements
+            .map { try? $0.map(to: AuthenticationModel.self) }
+            .map { $0?.accessToken ?? "" }
+            .do(onNext: { token in
+                keychainManager.set(key: .accessToken, value: token)
+                PictionManager.setToken(token)
+                updater.refreshSession.onNext(())
+            })
+            .map { _ in keychainManager.get(key: .pincode).isEmpty }
+
+        // 세션 생성 실패 시 badRequest이면 에러 필드에 메시지 출력
+        let errorMsg = signInAction.error
             .flatMap { response -> Driver<ErrorModel> in
                 let errorType = response as? ErrorType
                 switch errorType {
@@ -96,7 +130,8 @@ final class SignInViewModel: InjectableViewModel {
                 }
             }
 
-        let toastMessage = signInButtonAction.error
+        // 세션 생성 실패 시 badRequest가 아니면 토스트 메시지 출력
+        let toastMessage = signInAction.error
             .flatMap { response -> Driver<String> in
                 let errorType = response as? ErrorType
                 switch errorType {
@@ -107,23 +142,18 @@ final class SignInViewModel: InjectableViewModel {
                 }
             }
 
-        let signInSuccess = signInButtonAction.elements
-            .map { try? $0.map(to: AuthenticationModel.self) }
-            .map { $0?.accessToken ?? "" }
-            .do(onNext: { token in
-                keychainManager.set(key: .accessToken, value: token)
-                PictionManager.setToken(token)
-                updater.refreshSession.onNext(())
-            })
-            .map { _ in keychainManager.get(key: .pincode).isEmpty }
+        // 키보드로 인한 frame 변경 시
+        let keyboardWillChangeFrame = keyboardManager.keyboardWillChangeFrame
+            .asDriver(onErrorDriveWith: .empty())
 
-        let keyboardWillChangeFrame = keyboardManager.keyboardWillChangeFrame.asDriver(onErrorDriveWith: .empty())
+        // 로딩 뷰
+        let activityIndicator = signInAction.isExecuting
 
-        let activityIndicator = signInButtonAction.isExecuting
-
+        // 닫기 버튼 눌렀을 때
         let closeAction = input.closeBtnDidTap
             .map { false }
 
+        // 세션 생성 성공 또는 닫기 버튼 눌렀을 때 dismiss
         let dismissViewController = Driver.merge(signInSuccess, closeAction)
 
         return Output(
@@ -135,7 +165,7 @@ final class SignInViewModel: InjectableViewModel {
             openFindPassword: input.findPasswordBtnDidTap,
             keyboardWillChangeFrame: keyboardWillChangeFrame,
             dismissViewController: dismissViewController,
-            errorMsg: signInError,
+            errorMsg: errorMsg,
             toastMessage: toastMessage
         )
     }
