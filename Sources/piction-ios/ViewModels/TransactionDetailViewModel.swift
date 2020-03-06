0a95c78b46615a6bff0d96e0485f78affa167396
jhSeo <jh.seo@battleent.com>
2020-03-06 14:23:32 +0900

    Add comment for TransactionDetailViewModel

--- Sources/piction-ios/ViewModels/TransactionDetailViewModel.swift
+++ Sources/piction-ios/ViewModels/TransactionDetailViewModel.swift
@@ -10,6 +10,7 @@ import RxSwift
 import RxCocoa
 import PictionSDK
 
+// MARK: - TransactionDetailSection
 enum TransactionDetailSection {
     case info(transaction: TransactionModel)
     case header(title: String)
@@ -17,11 +18,13 @@ enum TransactionDetailSection {
     case footer
 }
 
+// MARK: - TransactionType
 enum TransactionType {
     case sponsorship
     case subscription
 }
 
+// MARK: - ViewModel
 final class TransactionDetailViewModel: InjectableViewModel {
 
     typealias Dependency = (
@@ -37,12 +40,14 @@ final class TransactionDetailViewModel: InjectableViewModel {
     }
 
     var loadRetryTrigger = PublishSubject<Void>()
+}
 
+// MARK: - Input & Output
+extension TransactionDetailViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
         let selectedIndexPath: Driver<IndexPath>
     }
-
     struct Output {
         let viewWillAppear: Driver<Void>
         let navigationTitle: Driver<String>
@@ -51,31 +56,42 @@ final class TransactionDetailViewModel: InjectableViewModel {
         let showErrorPopup: Driver<Void>
         let activityIndicator: Driver<Bool>
     }
+}
 
+// MARK: - ViewModel Build
+extension TransactionDetailViewModel {
     func build(input: Input) -> Output {
         let (firebaseManager, transaction) = (self.firebaseManager, self.transaction)
 
+        // 화면이 보여지기 전에
         let navigationTitle = input.viewWillAppear
             .map { transaction.inOut == "IN" ? LocalizationKey.menu_deposit_detail.localized() : LocalizationKey.menu_withdraw_detail.localized() }
             .do(onNext: { title in
+                // analytics screen event
                 firebaseManager.screenName("마이페이지_거래내역_\(title)")
             })
 
-        let initialLoad = input.viewWillAppear.asObservable().take(1).asDriver(onErrorDriveWith: .empty())
+        // 최초 진입 시
+        let initialLoad = input.viewWillAppear
+            .asObservable()
+            .take(1)
+            .asDriver(onErrorDriveWith: .empty())
 
-        let loadRetry = loadRetryTrigger.asDriver(onErrorDriveWith: .empty())
+        // 새로고침 필요 시
+        let loadRetry = loadRetryTrigger
+            .asDriver(onErrorDriveWith: .empty())
 
-        let transactionSponsorshipAction = Driver.merge(initialLoad, loadRetry)
+        // 최초 진입 시, 새로고침 필요 시
+        // 전달받은 transaction type이 sponsorship이면
+        // sponsorship transaction 정보 호출
+        let sponsorshipTransactionAction = Driver.merge(initialLoad, loadRetry)
             .filter { transaction.transactionType == "SPONSORSHIP" }
             .map { WalletAPI.sponsorshipTransaction(txHash: transaction.transactionHash ?? "") }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
-        let valueTypeInfo = input.viewWillAppear
-            .filter { transaction.transactionType == "VALUE_TRANSFER" }
-            .map { [TransactionDetailSection]() }
-
-        let transactionSponsorshipSuccess = transactionSponsorshipAction.elements
+        // sponsorship trasnaction 정보 호출 성공 시
+        let sponsorshipTransactionSuccess = sponsorshipTransactionAction.elements
             .map { try? $0.map(to: TransactionSponsorshipModel.self) }
             .map { [
                 TransactionDetailSection.header(title: transaction.inOut ?? "" == "IN" ? LocalizationKey.str_membership_sell_info.localized() : LocalizationKey.str_membership_buy_info.localized()),
@@ -86,8 +102,20 @@ final class TransactionDetailViewModel: InjectableViewModel {
                 TransactionDetailSection.footer,
             ] }
 
-        let sponsorshipTypeInfo = transactionSponsorshipSuccess
+        // sponsorship trasnaction 정보 호출 에러 시
+        let transactionInfoError = sponsorshipTransactionAction.error
+            .map { _ in Void() }
 
+        // sponsorship transaction 정보
+        let sponsorshipTypeInfo = sponsorshipTransactionSuccess
+
+        // 최초 진입 시
+        // transaction type이 VALUE_TRANSFER이면
+        let valueTypeInfo = initialLoad
+            .filter { transaction.transactionType == "VALUE_TRANSFER" }
+            .map { [TransactionDetailSection]() }
+
+        // transaction 정보 조합
         let transactionInfo = Driver.merge(valueTypeInfo, sponsorshipTypeInfo)
             .map { typeSection -> [TransactionDetailSection] in
                 var sections: [TransactionDetailSection] = [
@@ -110,12 +138,11 @@ final class TransactionDetailViewModel: InjectableViewModel {
             }
             .map { SectionType<TransactionDetailSection>.Section(title: "transactionInfo", items: $0) }
 
-        let transactionInfoError = transactionSponsorshipAction.error
-            .map { _ in Void() }
-
+        // 에러 팝업 출력
         let showErrorPopup = transactionInfoError
 
-        let activityIndicator = transactionSponsorshipAction.isExecuting
+        // 로딩 뷰
+        let activityIndicator = sponsorshipTransactionAction.isExecuting
 
         return Output(
             viewWillAppear: input.viewWillAppear,
