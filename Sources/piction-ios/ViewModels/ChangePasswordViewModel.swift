15d30eba0cb2cc62434a657764356b75b4ea06a6
jhSeo <jh.seo@battleent.com>
2020-03-06 14:36:34 +0900

    Add comment for ChangePasswordViewModel

--- Sources/piction-ios/ViewModels/ChangePasswordViewModel.swift
+++ Sources/piction-ios/ViewModels/ChangePasswordViewModel.swift
@@ -69,25 +69,40 @@ extension ChangePasswordViewModel {
                 keyboardManager.beginMonitoring()
             })
 
+        // 화면이 사라지기 전에
         let viewWillDisappear = input.viewWillDisappear
             .do(onNext: { _ in
                 // 키보드 모니터링 중단
                 keyboardManager.stopMonitoring()
             })
 
-        let changePasswordInfo = Driver.combineLatest(input.passwordTextFieldDidInput, input.newPasswordTextFieldDidInput, input.passwordCheckTextFieldDidInput) { (password: $0, newPassword: $1, passwordCheck: $2) }
+        // textField 입력 시
+        let changePasswordInfo = Driver.combineLatest(
+            input.passwordTextFieldDidInput,
+            input.newPasswordTextFieldDidInput,
+            input.passwordCheckTextFieldDidInput)
+            { (password: $0, newPassword: $1, passwordCheck: $2) }
 
+        // 모든 textField가 입력되면 저장 버튼 활성화
         let enableSaveButton = changePasswordInfo
             .filter { $0 != "" && $1 != "" && $2 != "" }
             .map { _ in Void() }
 
-        let saveButtonAction = input.saveBtnDidTap
+        // 저장 버튼 누르면
+        // 비밀번호 변경 호출
+        let changePasswordAction = input.saveBtnDidTap
             .withLatestFrom(changePasswordInfo)
             .map { UserAPI.updatePassword(password: $0.password, newPassword: $0.newPassword, passwordCheck: $0.passwordCheck) }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
-        let changePasswordError = saveButtonAction.error
+        // 비밀번호 변경 호출 성공 시
+        let changePasswordSuccess = changePasswordAction.elements
+            .map { _ in Void() }
+
+        // 비밀번호 변경 호출 에러 시
+        // badRequest이면 error 필드에 출력
+        let changePasswordError = changePasswordAction.error
             .flatMap { response -> Driver<ErrorModel> in
                 let errorMsg = response as? ErrorType
                 switch errorMsg {
@@ -98,16 +113,17 @@ extension ChangePasswordViewModel {
                 }
             }
 
-        let changePasswordSuccess = saveButtonAction.elements
-            .map { _ in Void() }
-
+        // 비밀번호 변경 에러 시
         let errorMsg = changePasswordError
 
         // 로딩 뷰
-        let activityIndicator = saveButtonAction.isExecuting
+        let activityIndicator = changePasswordAction.isExecuting
 
+        // 취소 버튼 눌렀을 때
         let dismissWithCancel = input.cancelBtnDidTap
 
+        // 비밀번호 변경 성공 시, 취소버튼 눌렀을 때
+        // dismiss
         let dismissViewController = Driver.merge(dismissWithCancel, changePasswordSuccess)
             .map { _ in Void() }
 
@@ -116,7 +132,8 @@ extension ChangePasswordViewModel {
             .asDriver(onErrorDriveWith: .empty())
 
         // 토스트 메시지
-        let toastMessage = saveButtonAction.error
+        // badRequest가 아닐 때
+        let toastMessage = changePasswordAction.error
             .flatMap { response -> Driver<String> in
                 let errorType = response as? ErrorType
                 switch errorType {
