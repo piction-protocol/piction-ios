50e27097f67bfb9118c62025b449b9beca5eb5c1
jhSeo <jh.seo@battleent.com>
2020-03-06 14:32:17 +0900

    Add comment for ChangeMyInfoViewModel

--- Sources/piction-ios/ViewModels/ChangeMyInfoViewModel.swift
+++ Sources/piction-ios/ViewModels/ChangeMyInfoViewModel.swift
@@ -92,34 +92,36 @@ extension ChangeMyInfoViewModel {
             .take(1)
             .asDriver(onErrorDriveWith: .empty())
 
+        // 최초 진입 시
+        // 유저 정보 호출
         let userInfoAction = initialLoad
             .map { UserAPI.me }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
+        // 유저 정보 호출 성공 시
         let userInfoSuccess = userInfoAction.elements
             .map { try? $0.map(to: UserModel.self) }
             .flatMap(Driver.from)
             .do(onNext: { [weak self] userInfo in
+                // 유저 정보 설정
                 self?.imageId.onNext("")
                 self?.email.onNext(userInfo.email ?? "")
                 self?.username.onNext(userInfo.username ?? "")
                 self?.changeInfo.onNext(false)
             })
 
+        // 프로필 이미지 선택 시
         let pictureBtnAction = input.pictureImageBtnDidTap
 
+        // image picker에서 이미지 선택 시
+        // 이미지 업로드 호출
         let uploadPictureAction = input.pictureImageDidPick
             .filter { $0 != nil }
             .map { $0! }
             .map { UserAPI.uploadPicture(image: $0) }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
-
-        let uploadPictureError = uploadPictureAction.error
-            .map { $0 as? ErrorType }
-            .map { $0?.message }
-            .flatMap(Driver.from)
 
         let uploadPictureSuccess = uploadPictureAction.elements
             .map { try? $0.map(to: StorageAttachmentModel.self) }
@@ -130,6 +132,13 @@ extension ChangeMyInfoViewModel {
                 self?.changeInfo.onNext(true)
             })
 
+        // 이미지 업로드 호출 에러 시
+        let uploadPictureError = uploadPictureAction.error
+            .map { $0 as? ErrorType }
+            .map { $0?.message }
+            .flatMap(Driver.from)
+
+        // 이미지 삭제 시
         let deletePicture = input.pictureImageDidPick
             .filter { $0 == nil }
             .map { _ in String?(nil) }
@@ -138,6 +147,11 @@ extension ChangeMyInfoViewModel {
                 self?.changeInfo.onNext(true)
             })
 
+        // 변경된 프로필 이미지
+        let changePicture = Driver.merge(uploadPictureSuccess, deletePicture)
+            .withLatestFrom(input.pictureImageDidPick)
+
+        // 이메일 입력 시
         let changedEmail = Driver.combineLatest(input.emailTextFieldDidInput, email.asDriver(onErrorDriveWith: .empty()))
             .do(onNext: { [weak self] inputEmail, email in
                 if inputEmail != "" && inputEmail != email {
@@ -148,6 +162,7 @@ extension ChangeMyInfoViewModel {
             })
             .map { $0 == "" ? $1 : $0 }
 
+        // 닉네임 입력 시
         let changedUsername = Driver.combineLatest(input.userNameTextFieldDidInput, username.asDriver(onErrorDriveWith: .empty()))
             .do(onNext: { [weak self] inputUsername, username in
                 if inputUsername != "" && inputUsername != username {
@@ -158,42 +173,53 @@ extension ChangeMyInfoViewModel {
             })
             .map { $0 == "" ? $1 : $0 }
 
-        let changePicture = Driver.merge(uploadPictureSuccess, deletePicture)
-            .withLatestFrom(input.pictureImageDidPick)
+        // 유저 정보가 변경되면
+        let changeUserInfo = Driver.combineLatest(
+            changedEmail,
+            changedUsername,
+            imageId.asDriver(onErrorDriveWith: .empty()))
+            { (email: $0, username: $1, imageId: $2) }
 
-        let changeUserInfo = Driver.combineLatest(changedEmail, changedUsername, imageId.asDriver(onErrorDriveWith: .empty())) { (email: $0, username: $1, imageId: $2) }
-
+        // 저장 버튼 활성화
         let enableSaveButton = changeInfo
             .asDriver(onErrorDriveWith: .empty())
 
+        // 저장 버튼 누르면 패스워드 팝업 출력
         let openPasswordPopup = input.saveBtnDidTap
 
-        let saveButtonAction = input.password
+        // 패스워드 입력 후
+        // 업데이트 호출
+        let updateAction = input.password
             .withLatestFrom(changeUserInfo) { (password: $0, userInfo: $1) }
             .filter { $0.password != nil }
             .map { UserAPI.update(email: $1.email, username: $1.username, password: $0 ?? "", picture: $1.imageId) }
             .map(PictionSDK.rx.requestAPI)
             .flatMap(Action.makeDriver)
 
-        let changeUserInfoError = saveButtonAction.error
-            .flatMap { response -> Driver<String> in
-                let errorType = response as? ErrorType
-                switch errorType {
-                case .badRequest(let error) where error.field == "email":
-                    return Driver.empty()
-                default:
-                    return Driver.just(errorType?.message ?? "")
-                }
-            }
-
-        let changeUserInfoSuccess = saveButtonAction.elements
+        // 업데이트 호출 성공 시
+        let changeUserInfoSuccess = updateAction.elements
             .map { _ in Void() }
             .flatMap(Driver.from)
             .do(onNext: { _ in
                 updater.refreshSession.onNext(())
             })
 
-        let showErrorLabel = saveButtonAction.error
+        // 업데이트 호출 에러 시
+        // badRequest이고 email 필드가 아니면
+        let changeUserInfoError = updateAction.error
+            .flatMap { response -> Driver<String> in
+                let errorType = response as? ErrorType
+                switch errorType {
+                case .badRequest(let error) where error.field == "email":
+                    return Driver.empty()
+                default:
+                    return Driver.just(errorType?.message ?? "")
+                }
+            }
+
+        // 업데이트 호출 에러 시
+        // badRequest이고 email 필드이면
+        let showErrorLabel = updateAction.error
             .flatMap { response -> Driver<String> in
                 let errorType = response as? ErrorType
                 switch errorType {
@@ -204,21 +230,26 @@ extension ChangeMyInfoViewModel {
                 }
             }
 
+        // emailTextField 입력 시 error 필드 숨김
         let hideErrorLabel = input.emailTextFieldDidInput
             .map { _ in Void() }
 
+        // 정보 변경 후 취소 버튼 누르면 경고 팝업 출력
         let openWarningPopup = input.cancelBtnDidTap
             .withLatestFrom(changeInfo.asDriver(onErrorDriveWith: .empty()))
             .filter { $0 }
             .map { _ in Void() }
             .flatMap(Driver.from)
 
+        // 정보 변경되지 않고 취소 버튼 누르면 dismiss
         let dismissWithCancel = input.cancelBtnDidTap
             .withLatestFrom(changeInfo.asDriver(onErrorDriveWith: .empty()))
             .filter { !$0 }
             .map { _ in Void() }
             .flatMap(Driver.from)
 
+        // 업데이트 호출 성공 시, 정보 변경 없이 취소 버튼 누를 때
+        // dismiss
         let dismissViewController = Driver.merge(dismissWithCancel, changeUserInfoSuccess)
 
         // 키보드로 인한 frame 변경 시
@@ -229,7 +260,7 @@ extension ChangeMyInfoViewModel {
         let activityIndicator = Driver.merge(
             userInfoAction.isExecuting,
             uploadPictureAction.isExecuting,
-            saveButtonAction.isExecuting)
+            updateAction.isExecuting)
 
         // 토스트 메시지
         let toastMessage = Driver.merge(
