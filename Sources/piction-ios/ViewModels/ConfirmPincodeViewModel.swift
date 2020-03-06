75c041024d6f8322d69937ce0a3234f73f3841b8
jhSeo <jh.seo@battleent.com>
2020-03-06 12:40:28 +0900

    Add comment for ConfirmPincodeViewModel

--- Sources/piction-ios/ViewModels/ConfirmPincodeViewModel.swift
+++ Sources/piction-ios/ViewModels/ConfirmPincodeViewModel.swift
@@ -9,6 +9,7 @@
 import RxSwift
 import RxCocoa
 
+// MARK: - ViewModel
 final class ConfirmPincodeViewModel: InjectableViewModel {
 
     typealias Dependency = (
@@ -21,38 +22,49 @@ final class ConfirmPincodeViewModel: InjectableViewModel {
     private let firebaseManager: FirebaseManagerProtocol
     private let updater: UpdaterProtocol
     private let keychainManager: KeychainManagerProtocol
-    private let inputedPincode: String
+    private let inputedPincode: String // CheckPincode에서 입력한 핀코드
 
     init(dependency: Dependency) {
         (firebaseManager, updater, keychainManager, inputedPincode) = dependency
     }
+}
 
+// MARK: - Input & Output
+extension ConfirmPincodeViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
         let pincodeTextFieldDidInput: Driver<String>
         let changeComplete: Driver<Void>
     }
-
     struct Output {
         let viewWillAppear: Driver<Void>
         let inputPincode: Driver<(String, String)>
         let dismissViewController: Driver<Void>
     }
+}
 
+// MARK: - ViewModel Build
+extension ConfirmPincodeViewModel {
     func build(input: Input) -> Output {
         let (firebaseManager, updater, keychainManager, inputedPincode) = (self.firebaseManager, self.updater, self.keychainManager, self.inputedPincode)
 
+        // 화면이 보여지기 전에
         let viewWillAppear = input.viewWillAppear
             .do(onNext: { _ in
+                // analytics screen event
                 firebaseManager.screenName("PIN재입력")
             })
 
+        // 핀코드 입력 시 현재 저장된 핀코드와 함꼐 전달
         let inputPincode = input.pincodeTextFieldDidInput
             .map { (inputedPincode, $0) }
 
+        // 핀코드 저장 완료 시
         let dismissViewController = input.changeComplete
             .do(onNext: { _ in
+                // 키체인에 저장
                 keychainManager.set(key: .pincode, value: inputedPincode)
+                // 세션 업데이트
                 updater.refreshSession.onNext(())
             })
 
