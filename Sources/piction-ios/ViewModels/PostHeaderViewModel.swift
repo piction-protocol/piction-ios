8fdbbaac9a9cc3b59c1b8e4fa50008547989251a
jhSeo <jh.seo@battleent.com>
2020-03-06 12:42:41 +0900

    Add comment for PostHeaderViewModel

--- Sources/piction-ios/ViewModels/PostHeaderViewModel.swift
+++ Sources/piction-ios/ViewModels/PostHeaderViewModel.swift
@@ -10,6 +10,7 @@ import RxSwift
 import RxCocoa
 import PictionSDK
 
+// MARK: - ViewModel
 final class PostHeaderViewModel: InjectableViewModel {
     typealias Dependency = (
         UpdaterProtocol,
@@ -24,23 +25,37 @@ final class PostHeaderViewModel: InjectableViewModel {
     init(dependency: Dependency) {
         (updater, postItem, userInfo) = dependency
     }
+}
 
+// MARK: - Input & Output
+extension PostHeaderViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
         let creatorBtnDidTap: Driver<Void>
     }
-
     struct Output {
         let headerInfo: Driver<(PostModel, UserModel)>
         let openCreatorProfileViewController: Driver<String>
     }
+}
 
+// MARK: - ViewModel Build
+extension PostHeaderViewModel {
     func build(input: Input) -> Output {
         let (postItem, userInfo) = (self.postItem, self.userInfo)
 
-        let headerInfo = input.viewWillAppear.asObservable().take(1).asDriver(onErrorDriveWith: .empty())
+        // 최초 진입 시
+        let initialLoad = input.viewWillAppear
+            .asObservable()
+            .take(1)
+            .asDriver(onErrorDriveWith: .empty())
+
+        // 최초 진입 시
+        // post정보와 유저 정보 전달
+        let headerInfo = initialLoad
             .map { (postItem, userInfo) }
 
+        // 크리에이터 눌렀을 때 크리에이터 정보 화면으로 이동
         let openCreatorProfileViewController = input.creatorBtnDidTap
             .map { userInfo.loginId }
             .flatMap(Driver.from)
