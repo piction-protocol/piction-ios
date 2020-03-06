930ad28df015929725bce117a286d00bb9c23fc7
jhSeo <jh.seo@battleent.com>
2020-03-06 12:43:35 +0900

    Add comment for RegisterPincodeViewModel

--- Sources/piction-ios/ViewModels/RegisterPincodeViewModel.swift
+++ Sources/piction-ios/ViewModels/RegisterPincodeViewModel.swift
@@ -9,6 +9,7 @@
 import RxSwift
 import RxCocoa
 
+// MARK: - ViewModel
 final class RegisterPincodeViewModel: InjectableViewModel {
 
     typealias Dependency = (
@@ -20,26 +21,34 @@ final class RegisterPincodeViewModel: InjectableViewModel {
     init(dependency: Dependency) {
         (firebaseManager) = dependency
     }
+}
 
+// MARK: - Input & Ouput
+extension RegisterPincodeViewModel {
     struct Input {
         let viewWillAppear: Driver<Void>
         let pincodeTextFieldDidInput: Driver<String>
         let closeBtnDidTap: Driver<Void>
     }
-
     struct Output {
         let viewWillAppear: Driver<Void>
         let pincodeText: Driver<String>
         let openRecommendPopup: Driver<Void>
     }
-
+}
+    
+// MARK: - ViewModel Build
+extension RegisterPincodeViewModel {
     func build(input: Input) -> Output {
         let firebaseManager = self.firebaseManager
 
+        // 화면이 보여지기 전에
         let viewWillAppear = input.viewWillAppear
             .do(onNext: { _ in
+                // analytics screen event
                 firebaseManager.screenName("새PIN설정")
             })
+
         return Output(
             viewWillAppear: viewWillAppear,
             pincodeText: input.pincodeTextFieldDidInput,
