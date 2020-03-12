278879e64e6570885bee8b20804d4672332d5fd2
jhSeo <jh.seo@battleent.com>
2020-03-12 14:56:42 +0900

    Add comments for HomeHeaderTableViewCell

--- Sources/piction-ios/Views/Cell/HomeHeaderTableViewCell.swift
+++ Sources/piction-ios/Views/Cell/HomeHeaderTableViewCell.swift
@@ -9,15 +9,19 @@
 import UIKit
 import PictionSDK
 
+// MARK: - ReuseTableViewCell
 final class HomeHeaderTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
 
     typealias Model = HomeHeaderType
+}
 
-    func configure(with model: Model) {
-        let type = model
+// MARK: - Public Method
+extension HomeHeaderTableViewCell {
+    func configure(with type: Model) {
 
+        // header 타입에 따라 문구 출력
         titleLabel.text = type == .notSubscribed ? LocalizationKey.str_subscribing_project_empty.localized() : LocalizationKey.str_post_empty.localized()
         descriptionLabel.text = type == .notSubscribed ? LocalizationKey.str_home_header_not_subscribing_subtitle.localized() : LocalizationKey.str_home_header_no_post_subtitle.localized()
     }
