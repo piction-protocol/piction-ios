fd6cc9320636996ed10bdbeb538c79dc35d4cf03
jhSeo <jh.seo@battleent.com>
2020-03-12 14:57:28 +0900

    Add comments for MembershipListTableViewCell

--- Sources/piction-ios/Views/Cell/MembershipListTableViewCell.swift
+++ Sources/piction-ios/Views/Cell/MembershipListTableViewCell.swift
@@ -9,12 +9,14 @@
 import UIKit
 import PictionSDK
 
+// MARK: - MembershipListTableViewCellModel
 struct MembershipListTableViewCellModel {
     let membership: MembershipModel
     let subscriptionInfo: SponsorshipModel?
     let postCount: Int
 }
 
+// MARK: - MembershipButtonStyle
 enum MembershipButtonStyle {
     case dimmed
     case `default`
@@ -43,7 +45,8 @@ enum MembershipButtonStyle {
     }
 }
 
-class MembershipListTableViewCell: ReuseTableViewCell {
+// MARK: - ReuseTableViewCell
+final class MembershipListTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var levelLabel: UILabel!
     @IBOutlet weak var subscriptionLimitLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
@@ -53,7 +56,10 @@ class MembershipListTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var subscriptionLabel: UILabel!
 
     typealias Model = MembershipListTableViewCellModel
+}
 
+// MARK: - Public Method
+extension MembershipListTableViewCell {
     func configure(with model: Model) {
         let (membership, subscriptionInfo, postCount) = (model.membership, model.subscriptionInfo, model.postCount)
 
@@ -61,22 +67,22 @@ class MembershipListTableViewCell: ReuseTableViewCell {
             if let expireDate = subscriptionInfo?.expireDate,
                 let membershipLevel = membership.level,
                 let subscriptionLevel = subscriptionInfo?.membership?.level,
-                membershipLevel == subscriptionLevel {
+                membershipLevel == subscriptionLevel { // 현재 멤버십 레벨과 현재 구독중인 멤버쉽 레벨이 같으면
                 return expireDate.toString(format: LocalizationKey.str_membership_expire.localized())
             }
             if let membershipSponsorLimit = membership.sponsorLimit,
                 let membershipSponsorCount = membership.sponsorCount,
-                (membershipSponsorLimit > 0 && (membershipSponsorCount >= membershipSponsorLimit)) || membership.sponsorLimit == 0 {
+                (membershipSponsorLimit > 0 && (membershipSponsorCount >= membershipSponsorLimit)) || membership.sponsorLimit == 0 { // 멤버십 구독자 수가 다 찼거나 같으면
                 return LocalizationKey.str_membership_not_avaliable.localized()
             }
             if let subscriptionLevel = subscriptionInfo?.membership?.level,
                 let membershipLevel = membership.level,
-                subscriptionLevel > membershipLevel {
+                subscriptionLevel > membershipLevel { // 구독중인 멤버십 레벨이 현재 멤버십 레벨보다 높으면
                 return LocalizationKey.str_membership_not_avaliable.localized()
             }
             if let membershipSponsorLimit = membership.sponsorLimit,
                 let membershipSponsorCount = membership.sponsorCount,
-                membershipSponsorLimit > 0 {
+                membershipSponsorLimit > 0 { // 현재 멤버십 레벨이 0보다 크면(유료 구독이면)
                 let remains = membershipSponsorLimit - membershipSponsorCount
                 return LocalizationKey.str_membership_remain.localized(with: remains)
             }
@@ -87,7 +93,7 @@ class MembershipListTableViewCell: ReuseTableViewCell {
             if let _ = subscriptionInfo?.expireDate,
                 let membershipLevel = subscriptionInfo?.membership?.level,
                 let subscriptionLevel = subscriptionInfo?.membership?.level,
-                membershipLevel == subscriptionLevel {
+                membershipLevel == subscriptionLevel { // 현재 멤버십과 구독중인 멤버십의 레벨이 같으면
                 return .pictionBlue
             } else {
                 return UIColor(r: 153, g: 153, b: 153)
@@ -97,17 +103,17 @@ class MembershipListTableViewCell: ReuseTableViewCell {
         var buttonStyle: MembershipButtonStyle {
             if let subscriptionLevel = subscriptionInfo?.membership?.level,
                 let membershipLevel = membership.level,
-                subscriptionLevel == membershipLevel {
+                subscriptionLevel == membershipLevel { // 현재 멤버십과 구독중인 멤버십의 레벨이 같으면
                 return .subscribing
             }
             if let subscriptionLevel = subscriptionInfo?.membership?.level,
                 let membershipLevel = membership.level,
-                subscriptionLevel > membershipLevel {
+                subscriptionLevel > membershipLevel { // 현재 멤버십보다 구독중인 멤버십의 레벨이 높으면
                 return .dimmed
             }
             if let membershipSponsorLimit = membership.sponsorLimit,
                 let membershipSponsorCount = membership.sponsorCount,
-                (membershipSponsorLimit > 0 && (membershipSponsorCount >= membershipSponsorLimit)) || membershipSponsorLimit == 0 {
+                (membershipSponsorLimit > 0 && (membershipSponsorCount >= membershipSponsorLimit)) || membershipSponsorLimit == 0 { // 멤버십 구독자 수가 다 찼거나 같으면
                 return .dimmed
             }
             return .default
@@ -116,11 +122,11 @@ class MembershipListTableViewCell: ReuseTableViewCell {
         var subscriptionText: String {
             if let subscriptionLevel = subscriptionInfo?.membership?.level,
                 let membershipLevel = membership.level,
-                subscriptionLevel == membershipLevel {
+                subscriptionLevel == membershipLevel { // 현재 멤버십과 구독중인 멤버십의 레벨이 같으면
                 return LocalizationKey.str_project_membership.localized()
             }
             if let price = membership.price,
-                price > 0 {
+                price > 0 { // 현재 멤버십 레벨이 0보다 크면(유료 구독이면)
                 return "\(price.commaRepresentation) \(LocalizationKey.str_membership_sponsorship_button.localized())"
             }
             return LocalizationKey.btn_subs_membership.localized()
