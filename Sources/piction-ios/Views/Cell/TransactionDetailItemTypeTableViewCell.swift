238df0aecbf2d51d2d2ed50efbd811b06f685c57
jhSeo <jh.seo@battleent.com>
2020-03-12 15:01:30 +0900

    Add comments for TransactionDetailItemTypeTableViewCell

--- Sources/piction-ios/Views/Cell/TransactionDetailItemTypeTableViewCell.swift
+++ Sources/piction-ios/Views/Cell/TransactionDetailItemTypeTableViewCell.swift
@@ -8,20 +8,27 @@
 
 import UIKit
 
+// MARK: - ReuseTableViewCell
 final class TransactionDetailItemTypeTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
 
     typealias Model = (String, String, String)
+}
 
+// MARK: - Public Method
+extension TransactionDetailItemTypeTableViewCell {
     func configure(with model: Model) {
         let (title, description, link) = model
 
         titleLabel.text = title
 
+        // 링크가 있는 경우
         if let _ = URL(string: link) {
             let linkString = NSMutableAttributedString(string: description)
+            // underline
             linkString.addAttribute(NSAttributedString.Key.underlineStyle, value: true, range: NSRange(location: 0, length: description.count))
+            // 색상을 pictionBlue로 변경
             linkString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.pictionBlue, range: NSRange(location: 0, length: description.count))
 
             descriptionLabel.attributedText = linkString
