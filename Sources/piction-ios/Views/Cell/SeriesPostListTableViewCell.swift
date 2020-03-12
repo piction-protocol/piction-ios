5fcaa009736b080eac8e7d40b4a0a9952d19349e
jhSeo <jh.seo@battleent.com>
2020-03-12 15:00:17 +0900

    Add comments for SeriesPostListTableViewCell

--- Sources/piction-ios/Views/Cell/SeriesPostListTableViewCell.swift
+++ Sources/piction-ios/Views/Cell/SeriesPostListTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 import PictionSDK
 
+// MARK: - ReuseTableViewCell
 final class SeriesPostListTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
@@ -16,12 +17,16 @@ final class SeriesPostListTableViewCell: ReuseTableViewCell {
 
     typealias Model = PostModel
 
+    // subview가 변경될 때
     override func layoutSubviews() {
         self.contentView.autoresizingMask = [.flexibleHeight]
 
         super.layoutSubviews()
     }
+}
 
+// MARK: - Public Method
+extension SeriesPostListTableViewCell {
     func configure(with model: Model, subscriptionInfo: SponsorshipModel?, number: Int) {
         let (title, date, membership) = (model.title, model.createdAt, model.membership)
 
@@ -29,6 +34,7 @@ final class SeriesPostListTableViewCell: ReuseTableViewCell {
 
         titleLabel.text = title
 
+        // 멤버십이 없거나 현재 멤버십의 레벨이 구독중인 멤버십 레벨보다 작으면
         if membership == nil || (subscriptionInfo?.membership?.level ?? 0) >= (membership?.level ?? 0) {
             subTitleLabel.textColor = .pictionGray
             subTitleLabel.text = date?.toString(format: LocalizationKey.str_series_date_format.localized())
