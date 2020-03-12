965343a18446aaba456d98db5d8dabf74caf5ac1
jhSeo <jh.seo@battleent.com>
2020-03-12 14:54:09 +0900

    Add comments for CategoryListCollectionViewCell

--- Sources/piction-ios/Views/Cell/CategoryListCollectionViewCell.swift
+++ Sources/piction-ios/Views/Cell/CategoryListCollectionViewCell.swift
@@ -9,23 +9,29 @@
 import UIKit
 import PictionSDK
 
+// MARK: - ReuseCollectionViewCell
 final class CategoryListCollectionViewCell: ReuseCollectionViewCell {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var categorizedCountLabel: UILabel!
     @IBOutlet weak var thumbnailMaskView: UIView!
 
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        thumbnailImageView.sd_cancelCurrentImageLoad()
-        thumbnailImageView.image = #imageLiteral(resourceName: "img-dummy-square-500-x-500")
-    }
-
     typealias Model = CategoryModel
 
+    // cell이 재사용 될 때
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.sd_cancelCurrentImageLoad() // 이미지 로딩 취소
+        thumbnailImageView.image = #imageLiteral(resourceName: "img-dummy-square-500-x-500") // 이미지를 placeholder 이미지로 교체
+    }
+}
+
+// MARK: - Public Method
+extension CategoryListCollectionViewCell {
     func configure(with model: Model) {
         let (thumbnail, title, categorizedCount) = (model.thumbnail, model.name, model.categorizedCount)
 
+        // 썸네일 출력
         if let thumbnail = thumbnail {
             let thumbnailWithIC = "\(thumbnail)?w=328&h=160&quality=80&output=webp"
             if let url = URL(string: thumbnailWithIC) {
