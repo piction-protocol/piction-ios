1c2f32544868896e6bcf0c88bda0f7c95a588246
jhSeo <jh.seo@battleent.com>
2020-03-12 14:59:35 +0900

    Add comments for ProjectPostListTypeListTableViewCell

--- Sources/piction-ios/Views/Cell/ProjectPostListTypeListTableViewCell.swift
+++ Sources/piction-ios/Views/Cell/ProjectPostListTypeListTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 import PictionSDK
 
+// MARK: - ReuseTableViewCell
 final class ProjectPostListTypeListTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var thumbnailView: UIView!
     @IBOutlet weak var thumbnailImageView: UIImageView!
@@ -21,20 +22,26 @@ final class ProjectPostListTypeListTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var maskImage: VisualEffectView!
     @IBOutlet weak var leftLockView: UIView!
 
+    // cell이 재사용 될 때
     override func prepareForReuse() {
         super.prepareForReuse()
-        thumbnailImageView.sd_cancelCurrentImageLoad()
-        thumbnailImageView.image = #imageLiteral(resourceName: "img-dummy-post-960-x-360")
+        thumbnailImageView.sd_cancelCurrentImageLoad() // 이미지 로딩 취소
+        thumbnailImageView.image = #imageLiteral(resourceName: "img-dummy-post-960-x-360") // 이미지를 placeholder 이미지로 교체
     }
 
+    // subview가 변경될 때
     override func layoutSubviews() {
         self.contentView.autoresizingMask = [.flexibleHeight]
         super.layoutSubviews()
     }
+}
 
+// MARK: - Public Method
+extension ProjectPostListTypeListTableViewCell {
     func configure(post: PostModel, subscriptionInfo: SponsorshipModel?, isWriter: Bool) {
         let (thumbnail, seriesName, title, publishedAt, likeCount, membership, status) = (post.cover, post.series?.name, post.title, post.publishedAt, post.likeCount, post.membership, post.status)
 
+        // 썸네일 출력
         if let thumbnail = thumbnail {
             thumbnailImageView.isHidden = false
             thumbnailView.backgroundColor = .pictionLightGray
@@ -63,6 +70,7 @@ final class ProjectPostListTypeListTableViewCell: ReuseTableViewCell {
         likeStackView.isHidden = (likeCount ?? 0) == 0
         likeLabel.text = "\(likeCount ?? 0)"
 
+        // 비공개는 writer인 경우만 나오므로 writer인 경우 잠금 표시를 보여주지 않음
         if status == "PRIVATE" {
             thumbnailView.isHidden = thumbnail == nil
             maskImage.isHidden = true
@@ -71,22 +79,26 @@ final class ProjectPostListTypeListTableViewCell: ReuseTableViewCell {
             lockView.backgroundColor = .clear
         } else {
             var needSubscription: Bool {
+                // 크리에이터는 구독이 필요하지 않음
                 if isWriter {
                     return false
                 }
+                // 멤버십이 없으면 구독이 필요하지 않음
                 if membership == nil {
                     return false
                 }
+                // 멤버십이 있지만 구독중인 멤버십이 없는 경우 구독 필요
                 if (membership?.level != nil) && (subscriptionInfo?.membership?.level == nil) {
                     return true
                 }
+                // 멤버십이 있고 구독중인 멤버십 레벨 보다 낮은 경우 구독이 필요하지 않음
                 if (membership?.level ?? 0) <= (subscriptionInfo?.membership?.level ?? 0) {
                     return false
                 }
                 return true
             }
 
-            if needSubscription {
+            if needSubscription { // 구독이 필요한 경우
                 thumbnailView.isHidden = thumbnail == nil
                 maskImage.isHidden = thumbnail == nil
                 lockView.isHidden = thumbnail == nil
@@ -94,7 +106,7 @@ final class ProjectPostListTypeListTableViewCell: ReuseTableViewCell {
 
                 maskImage.blurRadius = thumbnail == nil ? 0 : 5
                 lockView.backgroundColor = thumbnail == nil ? UIColor(r: 51, g: 51, b: 51, a: 0.2) : .clear
-            } else {
+            } else { // 구독이 필요하지 않은 경우
                 thumbnailView.isHidden = thumbnail == nil
                 leftLockView.isHidden = true
                 maskImage.isHidden = true
