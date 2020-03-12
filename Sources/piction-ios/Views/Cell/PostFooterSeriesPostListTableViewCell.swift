4ad97a003e9ae7de367ceab8b0c873e820af9e0b
jhSeo <jh.seo@battleent.com>
2020-03-12 14:59:02 +0900

    Add comments for PostFooterSeriesPostListTableViewCell

--- Sources/piction-ios/Views/Cell/PostFooterSeriesPostListTableViewCell.swift
+++ Sources/piction-ios/Views/Cell/PostFooterSeriesPostListTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 import PictionSDK
 
+// MARK: - ReuseTableViewCell
 final class PostFooterSeriesPostListTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
@@ -16,15 +17,21 @@ final class PostFooterSeriesPostListTableViewCell: ReuseTableViewCell {
 
     typealias Model = PostIndexModel
 
+    // subview가 변경될 때
     override func layoutSubviews() {
+        // height auto resizing
         self.contentView.autoresizingMask = [.flexibleHeight]
 
         super.layoutSubviews()
     }
+}
 
+// MARK: - Public Method
+extension PostFooterSeriesPostListTableViewCell {
     func configure(with model: Model, current: Bool) {
         let (number, title) = (model.index, model.post?.title)
 
+        // 현재 포스트와 같은 포스트면
         if current {
             numberLabel.isHidden = true
             currentImageContainerView.isHidden = false
