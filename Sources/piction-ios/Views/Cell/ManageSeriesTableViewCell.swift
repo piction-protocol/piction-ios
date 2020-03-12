17030bb58a7d3e25295a8d4b2a5afb6e41e3c9be
jhSeo <jh.seo@battleent.com>
2020-03-12 14:57:17 +0900

    Add comments for ManageSeriesTableViewCell

--- Sources/piction-ios/Views/Cell/ManageSeriesTableViewCell.swift
+++ Sources/piction-ios/Views/Cell/ManageSeriesTableViewCell.swift
@@ -9,14 +9,17 @@
 import UIKit
 import PictionSDK
 
+// MARK: - ReuseTableViewCell
 class ManageSeriesTableViewCell: ReuseTableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
 
     typealias Model = SeriesModel
+}
 
+// MARK: - Public Method
+extension ManageSeriesTableViewCell {
     func configure(with model: Model) {
         let (title) = (model.name)
-
         titleLabel.text = title
     }
 }
